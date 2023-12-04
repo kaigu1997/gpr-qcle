Change implementation of SH Remove animation but save each frame == gpr == Take some printing functions to utility.py Fix covariance calculation

evolve-py/main.py
@@ -289,41 +289,17 @@ def main() -> None:
 			np.savetxt(err_f, (original_errors[iTick], rescaled_errors[iTick]), footer='\n', comments='')
 			print(iTick, time.asctime(), flush=True)
 
-		def init() -> typing.Iterable[matplotlib.artist.Artist | typing.Iterable[matplotlib.artist.Artist]]:
-			"""
-			Initializer of the animation
-
-			Returns
-			-------
-			typing.Iterable[matplotlib.artist.Artist | typing.Iterable[matplotlib.artist.Artist]]
-				figure and axes
-			"""
-			for iPES in range(pes.NUM_PES):
-				for jPES in range(pes.NUM_PES):
-					ElementIndex: int = iPES * pes.NUM_PES + jPES
-					for iPlot in range(NUM_PLOTS):
-						ax: matplotlib.axes.Axes = axs[iPlot, ElementIndex]
-						ax.set_xlabel('x')
-						ax.set_ylabel('p')
-						ax.set_title('Rescaled ' + titles[iPlot] + get_RI_label(iPES, jPES))
-			fig.colorbar(matplotlib.cm.ScalarMappable(cmap=CMAP, norm=NORM), ax=axs.ravel().tolist())
-			pred_draw(0)
-			return fig, axs
-
-		def draw(iTick: int) -> typing.Iterable[matplotlib.artist.Artist | typing.Iterable[matplotlib.artist.Artist]]:
-			"""
-			Implementation of each frame
-
-			Parameters
-			----------
-			iTick : int
-				Current time tick, to access grid data and for plotting
-
-			Returns
-			-------
-			typing.Iterable[matplotlib.artist.Artist | typing.Iterable[matplotlib.artist.Artist]]
-				figure and axes
-			"""
+		for iPES in range(pes.NUM_PES):
+			for jPES in range(pes.NUM_PES):
+				ElementIndex: int = iPES * pes.NUM_PES + jPES
+				for iPlot in range(NUM_PLOTS):
+					ax: matplotlib.axes.Axes = axs[iPlot, ElementIndex]
+					ax.set_xlabel('x')
+					ax.set_ylabel('p')
+					ax.set_title('Rescaled ' + titles[iPlot] + get_RI_label(iPES, jPES))
+		fig.colorbar(matplotlib.cm.ScalarMappable(cmap=CMAP, norm=NORM), ax=axs.ravel().tolist())
+		pred_draw(0)
+		for iTick in range(1, total_ticks):
 			# evolve
 			for i in range(output_steps):
 				print(iTick, i, time.asctime(), flush=True)
@@ -353,10 +329,6 @@ def main() -> None:
 					interpolators[iElement][0].values = all_density[iElement].reshape(n_grids, n_grids).real
 					interpolators[iElement][1].values = all_density[iElement].reshape(n_grids, n_grids).imag
 			pred_draw(iTick)
-			return fig, axs
-
-		# draw animation
-		matplotlib.animation.FuncAnimation(fig, draw, range(1, total_ticks), init).save('diff.gif', 'imagemagick')
 
 
 if __name__ == "__main__":

evolve-py/sh.py
@@ -24,7 +24,6 @@ import main
 import pes
 import sample
 
-
 def main_func() -> None:
 	"""
 	The main routine
@@ -56,8 +55,8 @@ def main_func() -> None:
 	# possible destinations of fssh
 	dest_row_idx: npt.NDArray[np.int_] = np.concatenate(np.broadcast_arrays(pes.tril_row_indices[:, np.newaxis], np.arange(pes.NUM_PES)), -1) # i, k
 	dest_col_idx: npt.NDArray[np.int_] = np.concatenate(np.broadcast_arrays(np.arange(pes.NUM_PES), pes.tril_col_indices[:, np.newaxis]), -1) # k, j
-	coup_row_idx: npt.NDArray[np.int_] = dest_col_idx[:, np.concatenate((np.arange(pes.NUM_PES, 2 * pes.NUM_PES), np.arange(pes.NUM_PES)))] # j, k
-	coup_col_idx: npt.NDArray[np.int_] = dest_row_idx[:, np.concatenate((np.arange(pes.NUM_PES, 2 * pes.NUM_PES), np.arange(pes.NUM_PES)))] # k, i
+	coup_row_idx: npt.NDArray[np.int_] = np.repeat(np.tile(np.arange(pes.NUM_PES), 2)[np.newaxis], pes.NUM_TRIG, 0) # k, k
+	coup_col_idx: npt.NDArray[np.int_] = np.repeat(np.concatenate((pes.tril_col_indices[:, np.newaxis], pes.tril_row_indices[:, np.newaxis]), -1), pes.NUM_PES, -1) # j, i
 	filters: npt.NDArray[np.bool_] = np.logical_or(dest_row_idx != pes.tril_row_indices[:, np.newaxis], dest_col_idx != pes.tril_col_indices[:, np.newaxis]) # remove identical
 	dest_row_idx = dest_row_idx[filters].reshape(pes.tril_element_indices.size, 2 * pes.NUM_PES - 2)
 	dest_col_idx = dest_col_idx[filters].reshape(pes.tril_element_indices.size, 2 * pes.NUM_PES - 2)
@@ -74,42 +73,44 @@ def main_func() -> None:
 		for iTick in range(total_ticks):
 			for _ in range(output_steps):
 				# save current indices
+				x0: npt.NDArray[np.double] = pts[:, :pes.DIM] # N * D
+				p0: npt.NDArray[np.double] = pts[:, pes.DIM:] # N * D
+				x2: npt.NDArray[np.double] = np.empty_like(x0) # N * D
+				p1: npt.NDArray[np.double] = np.empty_like(p0) # N * D
+				x4: npt.NDArray[np.double] = np.empty_like(x0) # N * D
+				p2: npt.NDArray[np.double] = np.empty_like(p0) # N * D
 				current_indices: npt.NDArray[np.bool_] = belonging_idx == pes.tril_element_indices[:, np.newaxis]
-				for iTrig in range(pes.NUM_TRIG):
-					ElementIndex: int = pes.tril_element_indices[iTrig]
-					num_pts: int = np.count_nonzero(current_indices[iTrig])
-					if num_pts > 0: # only have elements
-						# evolve
-						pts[current_indices[iTrig], :pes.DIM], pts[current_indices[iTrig], pes.DIM:] = evolve.evolve_coordinates_adiabatically(
-							pts[current_indices[iTrig], :pes.DIM],
-							pts[current_indices[iTrig], pes.DIM:],
-							mass,
-							dt,
-							evolve.Direction.Forward,
-							ElementIndex // pes.NUM_PES,
-							ElementIndex % pes.NUM_PES
-						)
-						# hopping
-						# choose the one to jump to
-						idx_of_dest_idx: npt.NDArray[np.int_] = sample.np_rng.integers(2 * pes.NUM_PES - 2, size=num_pts) # n
-						velocity: npt.NDArray[np.double] = pts[current_indices[iTrig], pes.DIM:] / mass # n * D
-						coupling: npt.NDArray[np.double] = pes.adiabatic_coupling(pts[current_indices[iTrig], :pes.DIM])[np.arange(num_pts), :, coup_row_idx[iTrig, idx_of_dest_idx], coup_col_idx[iTrig, idx_of_dest_idx]] # n * D
-						transition_rate: npt.NDArray[np.double] = np.abs(np.sum(velocity * coupling, -1) * dt) # n
-						transition_prob: npt.NDArray[np.double] = transition_rate / (1.0 + transition_rate) # n
-						# energy conservation
-						potential: npt.NDArray[np.double] = pes.adiabatic_potential(pts[current_indices[iTrig], :pes.DIM]) # n * N
-						momentum_rescale_factor_sq: npt.NDArray[np.double] = 1.0\
-							+ np.where(
-								idx_of_dest_idx < pes.NUM_PES - 1,
-								potential[..., ElementIndex % pes.NUM_PES] - potential[np.arange(num_pts), dest_col_idx[iTrig, idx_of_dest_idx]], # diff on column, ij->ik
-								potential[..., ElementIndex // pes.NUM_PES] - potential[np.arange(num_pts), dest_row_idx[iTrig, idx_of_dest_idx]] # diff on row, ij->kj
-							)\
-							/ np.sum(pts[current_indices[iTrig], pes.DIM:] ** 2 / mass, -1) # n
-						# judgment
-						transition: npt.NDArray[np.bool_] = np.logical_and(sample.np_rng.random(num_pts) < transition_prob, momentum_rescale_factor_sq >= 0.0) # n
-						# chaneg index and momentum
-						belonging_idx[current_indices[iTrig]] = np.where(transition, dest_idx[iTrig, idx_of_dest_idx], ElementIndex)
-						pts[current_indices[iTrig], pes.DIM:] *= np.sqrt(np.where(transition, momentum_rescale_factor_sq, 1.0))[:, np.newaxis]
+				for iBelongPES in range(pes.NUM_PES):
+					for jBelongPES in range(iBelongPES + 1):
+						TrilIndex = pes.flatten_tril_index[iBelongPES, jBelongPES]
+						indices: npt.NDArray[np.int_] = np.flatnonzero(current_indices[TrilIndex]) # n
+						num_pts: int = indices.size
+						if num_pts > 0: # only have elements
+							# evolve
+							x2[indices], p1[indices] = evolve.evolve_coordinates_adiabatically(x0[indices], p0[indices], mass, dt / 2.0, evolve.Direction.Forward, iBelongPES, jBelongPES)
+							x4[indices], p2[indices] = evolve.evolve_coordinates_adiabatically(x2[indices], p1[indices], mass, dt / 2.0, evolve.Direction.Forward, iBelongPES, jBelongPES)
+							# hopping
+							# check energy availability
+							num_states_avail: int = (1 if iBelongPES == jBelongPES else 2) * (pes.NUM_PES - 1)
+							state_prob: npt.NDArray[np.double] = np.zeros((num_pts, num_states_avail + 1))
+							# weights
+							velocity: npt.NDArray[np.double] = p2[indices] / mass # npt * D
+							coupling: npt.NDArray[np.double] = pes.adiabatic_coupling(x4[indices])[:, :, coup_row_idx[TrilIndex, :num_states_avail], coup_col_idx[TrilIndex, :num_states_avail]] # npt * D * nst
+							state_prob[:, :-1] = np.abs(np.sum(velocity[..., np.newaxis] * coupling, -2) * dt) # npt * nst, |v*d*dt|
+							# energy conservation rule
+							potential: npt.NDArray[np.double] = pes.adiabatic_potential(x4[indices]) # npt * N
+							momentum_rescale_factor_sq: npt.NDArray[np.double] = 1.0\
+								+ (potential[:, coup_col_idx[TrilIndex, :num_states_avail]] - potential[:, coup_row_idx[TrilIndex, :num_states_avail]]) / np.sum(pts[indices, pes.DIM:] ** 2 / mass, -1, keepdims=True) # npt * nst
+							state_prob[:, :-1] *= np.where(momentum_rescale_factor_sq >= 0, 1, 0)
+							state_prob[:, -1] = 1.0 # stay at original state
+							state_prob /= np.sum(state_prob, -1, keepdims=True) # normalize
+							# choose the one to jump to
+							idx_of_dest_idx: npt.NDArray[np.int_] = np.array([sample.np_rng.choice(num_states_avail + 1, p=p) for p in state_prob])
+							# chaneg index and momentum
+							transition: npt.NDArray[np.bool_] = idx_of_dest_idx != num_states_avail
+							belonging_idx[indices[transition]] = dest_idx[TrilIndex, idx_of_dest_idx[transition]]
+							p2[indices[transition]] *= np.sqrt(momentum_rescale_factor_sq[transition, idx_of_dest_idx[transition]])[:, np.newaxis]
+							pts[indices, :pes.DIM], pts[indices, pes.DIM:] = x4[indices], p2[indices]
 			# print to file
 			np.savetxt(pts_f, pts.T, footer='\n', comments='')
 			np.savetxt(bln_f, belonging_idx[np.newaxis], footer='\n', comments='')
@@ -129,9 +130,8 @@ def main_func() -> None:
 	fig.colorbar(matplotlib.cm.ScalarMappable(cmap=main.CMAP, norm=main.NORM), ax=axs.ravel().tolist())
 	all_pts: npt.NDArray[np.double] = np.loadtxt("sh-points.txt").reshape(total_ticks, pes.PHASEDIM, main.NUM_PTS * pes.NUM_TRIG)
 	all_belonging: npt.NDArray[np.int_] = np.loadtxt("sh-belonging.txt").astype(np.int_)
-
-	def draw(iTick: int) -> typing.Iterable[matplotlib.artist.Artist | typing.Iterable[matplotlib.artist.Artist]]:
-		print("{}/{}".format(iTick, total_ticks), datetime.datetime.now())
+	for iTick in range(total_ticks):
+		print("{}/{}".format(iTick + 1, total_ticks), datetime.datetime.now())
 		ct_data: npt.NDArray[np.double] = main.real_part(data[iTick])
 		ct_data *= main.get_scale(ct_data)[:, np.newaxis, np.newaxis]
 		for iElement in range(pes.NUM_ELM):
@@ -156,11 +156,6 @@ def main_func() -> None:
 					)
 					ax.set_title("{} Points".format(np.count_nonzero(all_belonging[iTick] == max(iElement // pes.NUM_PES, iElement % pes.NUM_PES) * pes.NUM_PES + min(iElement // pes.NUM_PES, iElement % pes.NUM_PES))))
 		fig.savefig('Tick = {:03}.png'.format(iTick))
-		return fig, axs
-
-	# draw animation
-	matplotlib.animation.FuncAnimation(fig, draw, total_ticks).save('gpr.gif', 'imagemagick')
-
 	with tarfile.open('ticks.tar.gz', 'w:gz') as tf:
 		for iTick in range(total_ticks):
 			name: str = 'Tick = {:03}.png'.format(iTick)

gpr/evolve.py
@@ -168,6 +168,7 @@ def evolve_density_adiabatically(
 	density: npt.NDArray[np.cdouble],
 	x0: npt.NDArray[np.double],
 	x2: npt.NDArray[np.double],
+	x4: npt.NDArray[np.double] | None,
 	drc: Direction,
 	dt: float,
 	RowIndex: int,
@@ -183,7 +184,9 @@ def evolve_density_adiabatically(
 	x0 : npt.NDArray[np.double], shape of (..., DIM)
 		The initial positions
 	x2 : npt.NDArray[np.double], shape of (..., DIM)
-		The final positions
+		The final positions if x4 does not exist, otherwise the intermediate
+	x4 : npt.NDArray[np.double], shape of (..., DIM)
+		The final positions if exists
 	drc : Direction
 		The direction of evolution
 	dt : float
@@ -196,7 +199,11 @@ def evolve_density_adiabatically(
 	if RowIndex != ColIndex:
 		E0: npt.NDArray[np.double] = pes.adiabatic_potential(x0)
 		E2: npt.NDArray[np.double] = pes.adiabatic_potential(x2)
-		density *= np.exp(-drc.value * dt / 2.0 / pes.HBAR * 1.0j * (E0[..., RowIndex] - E0[..., ColIndex] + E2[..., RowIndex] - E2[..., ColIndex]))
+		if x4 is not None:
+			E4: npt.NDArray[np.double] = pes.adiabatic_potential(x4)
+			density *= np.exp(-drc.value * dt / 4.0 / pes.HBAR * 1.0j * (E0[..., RowIndex] - E0[..., ColIndex] + 2.0 * (E2[..., RowIndex] - E2[..., ColIndex]) + E4[..., RowIndex] - E4[..., ColIndex]))
+		else:
+			density *= np.exp(-drc.value * dt / 2.0 / pes.HBAR * 1.0j * (E0[..., RowIndex] - E0[..., ColIndex] + E2[..., RowIndex] - E2[..., ColIndex]))
 
 
 def evolve_density_non_adiabatically(
@@ -300,7 +307,7 @@ def evolve_density_non_adiabatically(
 				if branch_row_index == RowIndex and branch_col_index == ColIndex and density is not None:
 					rho_predict[index, evolve_density_non_adiabatically.offdiagonal_zero_branch_index] = density
 				# first half-step adiabatic evolve. (x4, p3) -> (x2, p2) with an adiabatic rotation
-				evolve_density_adiabatically(rho_predict[index], x2, x4[index], Direction.Forward, dt / 2.0, branch_row_index, branch_col_index)
+				evolve_density_adiabatically(rho_predict[index], x2, x4[index], None, Direction.Forward, dt / 2.0, branch_row_index, branch_col_index)
 			rho_combined_offdiag: npt.NDArray[np.cdouble] = np.zeros((pes.NUM_TRIG,) + rho_predict.shape[2:], np.cdouble) # NUM_TRIG * ...
 			for index, branch in enumerate(evolve_density_non_adiabatically.offdiagonal_branches):
 				# now they are at (x2, p2). A off-diagonal rotation is needed.
@@ -326,7 +333,7 @@ def evolve_density_non_adiabatically(
 			offdiagonal_rotation(rho_combined_offdiag, x2, p1, dt / 2.0)
 			# another adiabatic step, (x2, p1) -> (x0, p0)
 			trig_index: int = np.argwhere(pes.tril_element_indices == RowIndex * pes.NUM_PES + ColIndex)[0, 0]
-			evolve_density_adiabatically(rho_combined_offdiag[trig_index], x0, x2, Direction.Forward, dt / 2.0, RowIndex, ColIndex)
+			evolve_density_adiabatically(rho_combined_offdiag[trig_index], x0, x2, None, Direction.Forward, dt / 2.0, RowIndex, ColIndex)
 			return rho_combined_offdiag[trig_index]
 		case _:
 			raise NotImplementedError('Model NOT Implemented!')
@@ -344,8 +351,7 @@ def evolve_element_density(
 	dt: float,
 	predictor: typing.Callable[[npt.NDArray[np.double], int], npt.NDArray[np.cdouble]],
 	RowIndex: int,
-	ColIndex: int,
-	CopyDensity: bool = False
+	ColIndex: int
 ) -> None:
 	"""
 	To evolve the density and phase space coordinates.
@@ -378,18 +384,14 @@ def evolve_element_density(
 		Index of row of the element in density matrix
 	ColIndex : int
 		Index of column of the element in density matrix
-	CopyDensity : bool, optional
-		Whether to copy the density and assign back later (True), or operate on density directly (False)
 	"""
 	evolve_element_density.drc = Direction.Forward
 	# judge coupling
 	IsCouple: npt.NDArray[np.bool_] = is_coupling(x4, p2, mass, dt) # M * D
 	IsCouplePerPoint: npt.NDArray[np.bool_] = np.any(IsCouple, -1) # M
 	adiabatic_indices: npt.NDArray[np.int_] = np.flatnonzero(np.logical_not(IsCouplePerPoint)) # adiabatic points
-	x2_adiabatic: npt.NDArray[np.double] = x2.reshape(-1, pes.DIM)[adiabatic_indices]
 	non_adiabatic_indices: npt.NDArray[np.int_] = np.flatnonzero(IsCouplePerPoint) # non-adiabatic points
-	evolve_density_adiabatically(density.ravel()[adiabatic_indices], x0.reshape(-1, pes.DIM)[adiabatic_indices], x2_adiabatic, evolve_element_density.drc, dt / 2.0, RowIndex, ColIndex)
-	evolve_density_adiabatically(density.ravel()[adiabatic_indices], x2_adiabatic, x4.reshape(-1, pes.DIM)[adiabatic_indices], evolve_element_density.drc, dt / 2.0, RowIndex, ColIndex)
+	evolve_density_adiabatically(density.ravel()[adiabatic_indices], x0.reshape(-1, pes.DIM)[adiabatic_indices], x2.reshape(-1, pes.DIM)[adiabatic_indices], x4.reshape(-1, pes.DIM)[adiabatic_indices], evolve_element_density.drc, dt, RowIndex, ColIndex)
 	density[non_adiabatic_indices] = evolve_density_non_adiabatically(
 		density[non_adiabatic_indices],
 		x4.reshape(-1, pes.DIM)[non_adiabatic_indices],
@@ -457,7 +459,7 @@ def sh_evolve(
 	predictor: typing.Callable[[npt.NDArray[np.double], int], npt.NDArray[np.cdouble]]
 ) -> None:
 	"""
-	_summary_
+	Non-adiabatic dynamics with surface hopping
 
 	Parameters
 	----------
@@ -466,7 +468,7 @@ def sh_evolve(
 	densities : npt.NDArray[np.cdouble], shape of (NUM_TRIG * NUM_PTS,)
 		Density matrix element of the points
 	belonging_idx : npt.NDArray[np.int_], shape of (NUM_TRIG * NUM_PTS,)
-
+		The index of density matrix element where the points belong to
 	mass : npt.NDArray[np.double], shape of (DIM,)
 		Mass of classical degree of freedom
 	dt : float
@@ -485,61 +487,64 @@ def sh_evolve(
 	for iBelongPES in range(pes.NUM_PES):
 		for jBelongPES in range(iBelongPES + 1):
 			TrilIndex = pes.flatten_tril_index[iBelongPES, jBelongPES]
-			indices: npt.NDArray[np.int_] = np.flatnonzero(current_indices[TrilIndex]) # n
-			# get x and p, and 2 semi adiabatic steps
-			x2[indices], p1[indices] = evolve_coordinates_adiabatically(x0[indices], p0[indices], mass, dt / 2.0, Direction.Forward, iBelongPES, jBelongPES)
-			x4[indices], p2[indices] = evolve_coordinates_adiabatically(x2[indices], p1[indices], mass, dt / 2.0, Direction.Forward, iBelongPES, jBelongPES)
-			# surface hopping
-			# choose the one to jump to
+			indices: npt.NDArray[np.int_] = np.flatnonzero(current_indices[TrilIndex]) # npt
 			num_pts: int = indices.size
-			idx_of_dest_idx: npt.NDArray[np.int_] = sample.np_rng.integers(0, 2 * pes.NUM_PES - 2, size=num_pts, dtype=np.int_) # n
-			velocity: npt.NDArray[np.double] = p2[indices] / mass # n * D
-			coupling: npt.NDArray[np.double] = pes.adiabatic_coupling(x4[indices])[np.arange(num_pts), :, coup_row_idx[TrilIndex, idx_of_dest_idx], coup_col_idx[TrilIndex, idx_of_dest_idx]] # n * D
-			transition_rate: npt.NDArray[np.double] = np.abs(np.sum(velocity * coupling, -1) * dt) # n
-			transition_prob: npt.NDArray[np.double] = transition_rate / (1.0 + transition_rate) # n
-			# energy conservation
-			potential: npt.NDArray[np.double] = pes.adiabatic_potential(x4[indices]) # n * N
-			momentum_rescale_factor_sq: npt.NDArray[np.double] = 1.0\
-				+ (potential[np.arange(num_pts), coup_col_idx[TrilIndex, idx_of_dest_idx]] - potential[np.arange(num_pts), coup_row_idx[TrilIndex, idx_of_dest_idx]]) / np.sum(p2[indices] ** 2 / mass, -1) # n
-			# judgment
-			transition: npt.NDArray[np.bool_] = np.logical_and(sample.np_rng.random(num_pts) < transition_prob, momentum_rescale_factor_sq >= 0.0) # n
-			# change index, momentum, and the back propagation
-			if np.any(transition):
-				belonging_idx[indices[transition]] = dest_idx[TrilIndex, idx_of_dest_idx[transition]] # indices[transition] equivalent to [current_indices[TrilIndex]][transition]
-				p2[indices[transition]] *= np.sqrt(momentum_rescale_factor_sq[transition])[:, np.newaxis]
-				# change density
-				for iPredictPES in range(pes.NUM_PES):
-					for jPredictPES in range(iPredictPES + 1):
-						if iBelongPES == iPredictPES and jBelongPES == jPredictPES:
-							pass
-						PredictElementIndex: int = iPredictPES * pes.NUM_PES + jPredictPES
-						need_predict: npt.NDArray[np.bool_] = np.logical_and(transition, dest_idx[TrilIndex, idx_of_dest_idx] == PredictElementIndex) # in case the transition happens to this element
-						if np.any(need_predict):
-							x2[indices[need_predict]], p1[indices[need_predict]] = evolve_coordinates_adiabatically(x4[indices[need_predict]], p2[indices[need_predict]], mass, dt / 2.0, Direction.Backward, iPredictPES, jPredictPES)
-							x0[indices[need_predict]], p0[indices[need_predict]] = evolve_coordinates_adiabatically(x2[indices[need_predict]], p1[indices[need_predict]], mass, dt / 2.0, Direction.Backward, iPredictPES, jPredictPES)
-							densities[indices[need_predict]] = predictor(points[indices[need_predict]], PredictElementIndex)
+			if num_pts > 0: # only have elements
+				# get x and p, and 2 semi adiabatic steps
+				x2[indices], p1[indices] = evolve_coordinates_adiabatically(x0[indices], p0[indices], mass, dt / 2.0, Direction.Forward, iBelongPES, jBelongPES)
+				x4[indices], p2[indices] = evolve_coordinates_adiabatically(x2[indices], p1[indices], mass, dt / 2.0, Direction.Forward, iBelongPES, jBelongPES)
+				# surface hopping
+				num_states_avail: int = (1 if iBelongPES == jBelongPES else 2) * (pes.NUM_PES - 1)
+				state_prob: npt.NDArray[np.double] = np.zeros((num_pts, num_states_avail + 1))
+				# weights
+				velocity: npt.NDArray[np.double] = p2[indices] / mass # npt * D
+				coupling: npt.NDArray[np.double] = pes.adiabatic_coupling(x4[indices])[..., coup_row_idx[TrilIndex, :num_states_avail], coup_col_idx[TrilIndex, :num_states_avail]] # npt * D * nst
+				state_prob[:, :-1] = np.abs(np.sum(velocity[..., np.newaxis] * coupling, -2) * dt) # npt * nst, |v*d*dt|
+				# energy conservation rule
+				potential: npt.NDArray[np.double] = pes.adiabatic_potential(x4[indices]) # npt * N
+				momentum_rescale_factor_sq: npt.NDArray[np.double] = 1.0\
+					+ (potential[:, coup_col_idx[TrilIndex, :num_states_avail]] - potential[:, coup_row_idx[TrilIndex, :num_states_avail]]) / np.sum(p2[indices] ** 2 / mass, -1, keepdims=True) # npt * nst
+				state_prob[:, :-1] *= np.where(momentum_rescale_factor_sq >= 0.0, 1.0, 0.0)
+				state_prob[:, -1] = 1.0 # stay at original state
+				state_prob /= np.sum(state_prob, -1, keepdims=True) # normalize
+				# choose the one to jump to
+				idx_of_dest_idx: npt.NDArray[np.int_] = np.array([sample.np_rng.choice(num_states_avail + 1, p=p) for p in state_prob])
+				# change index, momentum, and the back propagation
+				transition: npt.NDArray[np.bool_] = idx_of_dest_idx != num_states_avail
+				if np.any(transition):
+					belonging_idx[indices[transition]] = dest_idx[TrilIndex, idx_of_dest_idx[transition]] # indices[transition] equivalent to [current_indices[TrilIndex]][transition]
+					p2[indices[transition]] *= np.sqrt(momentum_rescale_factor_sq[transition, idx_of_dest_idx[transition]])[:, np.newaxis]
+					# change density
+					for iPredictPES in range(pes.NUM_PES):
+						for jPredictPES in range(iPredictPES + 1):
+							if iBelongPES == iPredictPES and jBelongPES == jPredictPES:
+								pass
+							PredictElementIndex: int = iPredictPES * pes.NUM_PES + jPredictPES
+							need_predict: npt.NDArray[np.bool_] = dest_idx[TrilIndex, idx_of_dest_idx[transition]] == PredictElementIndex # in case the transition happens to this element
+							if np.any(need_predict):
+								x2[indices[transition][need_predict]], p1[indices[transition][need_predict]] = evolve_coordinates_adiabatically(x4[indices[transition][need_predict]], p2[indices[transition][need_predict]], mass, dt / 2.0, Direction.Backward, iPredictPES, jPredictPES)
+								x0[indices[transition][need_predict]], p0[indices[transition][need_predict]] = evolve_coordinates_adiabatically(x2[indices[transition][need_predict]], p1[indices[transition][need_predict]], mass, dt / 2.0, Direction.Backward, iPredictPES, jPredictPES)
+								densities[indices[transition][need_predict]] = predictor(points[indices[transition][need_predict]], PredictElementIndex)
 	# evolve density
-	current_indices: npt.NDArray[np.bool_] = belonging_idx == pes.tril_element_indices[:, np.newaxis]
 	for iPES in range(pes.NUM_PES):
 		for jPES in range(iPES + 1):
-			TrilIndex = pes.flatten_tril_index[iPES, jPES]
-			den_copy: npt.NDArray[np.cdouble] = np.copy(densities[current_indices[TrilIndex]])
+			filters: npt.NDArray[np.bool_] = belonging_idx == iPES * pes.NUM_PES + jPES
+			density_copy: npt.NDArray[np.cdouble] = np.copy(densities[filters])
 			evolve_element_density(
-				x0[current_indices[TrilIndex]],
-				p0[current_indices[TrilIndex]],
-				x2[current_indices[TrilIndex]],
-				p1[current_indices[TrilIndex]],
-				x4[current_indices[TrilIndex]],
-				p2[current_indices[TrilIndex]],
-				den_copy,
+				x0[filters],
+				p0[filters],
+				x2[filters],
+				p1[filters],
+				x4[filters],
+				p2[filters],
+				density_copy,
 				mass,
 				dt,
 				predictor,
 				iPES,
-				jPES,
-				True
+				jPES
 			)
-			densities[current_indices[TrilIndex]] = den_copy
+			densities[filters] = density_copy
 			# finally set up the point coordinates
-			points[current_indices[TrilIndex], :pes.DIM] = x4[current_indices[TrilIndex]]
-			points[current_indices[TrilIndex], pes.DIM:] = p2[current_indices[TrilIndex]]
+			points[filters, :pes.DIM] = x4[filters]
+			points[filters, pes.DIM:] = p2[filters]

gpr/expectation.py
@@ -17,6 +17,9 @@ import gp
 import pes
 import sample
 
+PURITY_FACTOR: float = (2.0 * np.pi * pes.HBAR) ** pes.DIM
+
+
 class Averager(abc.ABC):
 	"""
 	To calculate averages
@@ -76,7 +79,8 @@ class Averager(abc.ABC):
 		npt.NDArray[np.double]
 			PHASEDIM-by-PHASEDIM covariance matrix
 		"""
-		return self.square_coordinates() - self.coordinates()[:, np.newaxis] * self.coordinates()[np.newaxis, :]
+		coord_ave: npt.NDArray[np.double] = self.coordinates()
+		return self.square_coordinates() + (self.population().sum() - 2.0) * coord_ave[:, np.newaxis] * coord_ave[np.newaxis, :]
 
 	def standard_deviation(self) -> npt.NDArray[np.double]:
 		"""
@@ -87,7 +91,7 @@ class Averager(abc.ABC):
 		npt.NDArray[np.double]
 			Standard deviation of each dimension
 		"""
-		return np.sqrt(np.diagonal(self.square_coordinates()) - self.coordinates() ** 2)
+		return np.sqrt(np.diagonal(self.covariance()))
 
 	@abc.abstractmethod
 	def potential(self) -> float:
@@ -184,36 +188,12 @@ class MonteCarloAverage(Averager):
 				self.density[TrilIndex] = predictor(self.point_set[TrilIndex], iPES * pes.NUM_PES + jPES)
 
 	def population(self) -> npt.NDArray[np.double]:
-		"""
-		To calculate the population on each potential energy surfaces
-
-		Returns
-		-------
-		npt.NDArray[np.double]
-			Population on each surfaces
-		"""
 		return np.average(self.density[MonteCarloAverage.DIAGONAL_TRIL_INDEX].real / self.weight[MonteCarloAverage.DIAGONAL_TRIL_INDEX], -1)
 
 	def coordinates(self) -> npt.NDArray[np.double]:
-		"""
-		To calculate the average of phase space coordinates
-
-		Returns
-		-------
-		npt.NDArray[np.double]
-			Average positions and momenta
-		"""
 		return np.sum(np.average(np.moveaxis(self.point_set[MonteCarloAverage.DIAGONAL_TRIL_INDEX], -1, 0) * self.density[MonteCarloAverage.DIAGONAL_TRIL_INDEX].real / self.weight[MonteCarloAverage.DIAGONAL_TRIL_INDEX], axis=-1), 1)
 
 	def square_coordinates(self) -> npt.NDArray[np.double]:
-		"""
-		To calculate averages of product of phase space coordinates
-
-		Returns
-		-------
-		npt.NDArray[np.double]
-			A PHASEDIM-by-PHASEDIM matrix, whose ij term is <x_i*x_j>
-		"""
 		return np.sum(
 			np.average(
 				self.density[MonteCarloAverage.DIAGONAL_TRIL_INDEX, :, np.newaxis, np.newaxis].real
@@ -225,28 +205,24 @@ class MonteCarloAverage(Averager):
 			0
 		)
 
-	def potential(self) -> float:
-		"""
-		To calculate the average potential energy
+	def covariance(self) -> npt.NDArray[np.double]:
+		coord_ave: npt.NDArray[np.double] = self.coordinates()
+		return np.sum(
+			np.average(
+				self.density[MonteCarloAverage.DIAGONAL_TRIL_INDEX, :, np.newaxis, np.newaxis].real
+				* (self.point_set[MonteCarloAverage.DIAGONAL_TRIL_INDEX, :, :, np.newaxis] - coord_ave[:, np.newaxis])
+				* (self.point_set[MonteCarloAverage.DIAGONAL_TRIL_INDEX, :, np.newaxis, :] - coord_ave[np.newaxis, :])
+				/ self.weight[MonteCarloAverage.DIAGONAL_TRIL_INDEX, :, np.newaxis, np.newaxis],
+				1
+			),
+			0
+		)
 
-		Returns
-		-------
-		float
-			Average potential energy
-		"""
+	def potential(self) -> float:
 		return np.sum(np.average(pes.adiabatic_potential(self.point_set[MonteCarloAverage.DIAGONAL_TRIL_INDEX, :, :pes.DIM])[np.arange(pes.NUM_PES), :, np.arange(pes.NUM_PES)] * self.density[MonteCarloAverage.DIAGONAL_TRIL_INDEX].real / self.weight[MonteCarloAverage.DIAGONAL_TRIL_INDEX], -1))
 
 	def purity(self) -> npt.NDArray[np.double]:
-		"""
-		To calculate contribution to purity of each element
-
-		Returns
-		-------
-		npt.NDArray[np.double]
-			Purity of each element
-		"""
-		result: npt.NDArray[np.double] = np.zeros((pes.NUM_PES, pes.NUM_PES), np.double)
-		return pes.PURITY_FACTOR * pes.lower_triangular_to_full(np.average((self.density.real ** 2 + self.density.imag ** 2) / self.weight, -1))
+		return PURITY_FACTOR * pes.lower_triangular_to_full(np.average((self.density.real ** 2 + self.density.imag ** 2) / self.weight, -1))
 
 
 class AnalyticalAverager(Averager):
@@ -269,14 +245,6 @@ class AnalyticalAverager(Averager):
 		self._predictors: gp.GPRPredictors = pred
 
 	def population(self) -> npt.NDArray[np.double]:
-		"""
-		To calculate the population on each potential energy surfaces
-
-		Returns
-		-------
-		npt.NDArray[np.double]
-			Population on each surfaces
-		"""
 		result: npt.NDArray[np.double] = np.empty(pes.NUM_PES, np.double)
 		for iPES in range(pes.NUM_PES):
 			ElementIndex: int = iPES * pes.NUM_PES + iPES
@@ -285,30 +253,14 @@ class AnalyticalAverager(Averager):
 		return result * AnalyticalAverager.AVERAGE_CONSTANT
 
 	def coordinates(self) -> npt.NDArray[np.double]:
-		"""
-		To calculate the average of phase space coordinates
-
-		Returns
-		-------
-		npt.NDArray[np.double]
-			Average positions and momenta
-		"""
 		result: npt.NDArray[np.double] = np.zeros(pes.PHASEDIM, np.double)
 		for iPES in range(pes.NUM_PES):
 			ElementIndex: int = iPES * pes.NUM_PES + iPES
 			pred: gp.SinglePredictor = self._predictors[ElementIndex]
-			result += pred.model.cov.lengthscale.prod().item() * (pred.get_weights().unsqueeze(-1) * pred.get_training_features()).sum(0).detach().numpy() / self._predictors.scale[ElementIndex]
+			result += pred.model.cov.lengthscale.prod().item() / self._predictors.scale[ElementIndex] * (pred.get_weights()[:, None] * pred.get_training_features()).sum(0).detach().numpy()
 		return result * AnalyticalAverager.AVERAGE_CONSTANT
 
 	def square_coordinates(self) -> npt.NDArray[np.double]:
-		"""
-		To calculate averages of product of phase space coordinates
-
-		Returns
-		-------
-		npt.NDArray[np.double]
-			A PHASEDIM-by-PHASEDIM matrix, whose ij term is <x_i*x_j>
-		"""
 		result: npt.NDArray[np.double] = np.zeros((pes.PHASEDIM, pes.PHASEDIM))
 		for iPES in range(pes.NUM_PES):
 			ElementIndex: int = iPES * pes.NUM_PES + iPES
@@ -318,26 +270,13 @@ class AnalyticalAverager(Averager):
 				+ pred.get_weights().sum() * torch.diagflat(pred.model.cov.lengthscale ** 2)).detach().numpy()
 		return result * AnalyticalAverager.AVERAGE_CONSTANT
 
-	def potential(self) -> float:
-		"""
-		To calculate the average potential energy
+	def covariance(self) -> npt.NDArray[np.double]:
+		return super().covariance()
 
-		Returns
-		-------
-		float
-			Average potential energy
-		"""
+	def potential(self) -> float:
 		return np.nan
 
 	def purity(self) -> npt.NDArray[np.double]:
-		"""
-		To calculate contribution to purity of each element
-
-		Returns
-		-------
-		npt.NDArray[np.double]
-			Purity of each element
-		"""
 		result: npt.NDArray[np.double] = np.empty((pes.NUM_PES, pes.NUM_PES), np.double)
 		for iPES in range(pes.NUM_PES):
 			for jPES in range(pes.NUM_PES):
@@ -345,4 +284,4 @@ class AnalyticalAverager(Averager):
 				model: gp.GP = copy.deepcopy(pred.model)
 				model.cov.lengthscale *= np.sqrt(2.0)
 				result[iPES, jPES] = (np.pi ** pes.DIM) * pred.model.cov.lengthscale.prod().item() * (pred.get_weights() @ model.cov(pred.get_training_features(), pred.get_training_features()) @ pred.get_weights()).item() / (self._predictors.scale[iPES * pes.NUM_PES + jPES] ** 2)
-		return pes.PURITY_FACTOR * (result + result.T - np.diag(np.diag(result)))
+		return PURITY_FACTOR * (result + result.T - np.diag(np.diag(result)))

gpr/gp.py
@@ -17,35 +17,10 @@ import numpy.typing as npt
 import torch
 
 import pes
+import utility
 
 torch.set_default_dtype(torch.double)
 torch.manual_seed(0)
-
-
-def get_RI_label(ElementIndex: int) -> str:
-	"""
-	To have the real/imaginary part name of the given input row and column.
-
-	Strictly-upper triangular is real, strictly-lower triangular is imaginary, and diagonal elements are as they are.
-
-	Parameters
-	----------
-	ElementIndex: int
-		Index of the element
-
-	Returns
-	-------
-	str
-		Name of the real/imaginary part. 
-	"""
-	RowIndex: int = ElementIndex // pes.NUM_PES
-	ColIndex: int = ElementIndex % pes.NUM_PES
-	if RowIndex == ColIndex:
-		return "$\\rho_{{{},{}}}$".format(RowIndex, ColIndex)
-	elif RowIndex < ColIndex:
-		return "$\\Re\\rho_{{{},{}}}$".format(ColIndex, RowIndex)
-	else:
-		return "$\\Im\\rho_{{{},{}}}$".format(RowIndex, ColIndex)
 
 
 class GP(gpytorch.models.ExactGP):
@@ -225,35 +200,18 @@ class SinglePredictor:
 			print_grad : bool, optional
 				Whether to print the gradient or not, by default False
 			"""
-			def make_tensor_printable(t: torch.Tensor) -> float | npt.NDArray[np.double]:
-				"""
-				To transform a torch Tensor to read-friendly form
-
-				If the tensor contains only 1 element, return the element;
-				otherwise, return the flattened numpy array
-
-				Parameters
-				----------
-				t : torch.Tensor
-					The tensor
-
-				Returns
-				-------
-				float | npt.NDArray[np.double]
-					Return the only element or the flattened array
-				"""
-				if t.dim() == 0 or np.prod(t.shape) == 1:
-					return t.item()
-				else:
-					return t.detach().numpy().ravel()
-
-			fmt: str = "Parameter name: {0:42} value = {1}"
-			fmt_grad: str = fmt + " grad = {2}"
 			for param_name, param, constraint in model.named_parameters_and_constraints():
 				if print_grad and param.grad is not None:
-					print(fmt_grad.format(param_name, make_tensor_printable(param), make_tensor_printable(param.grad)))
+					print(
+						"Parameter name: {}".format(param_name),
+						utility.format_array("value", param),
+						utility.format_array("grad", param.grad)
+					)
 				else:
-					print(fmt.format("".join(param_name.split("raw_")), make_tensor_printable(constraint.transform(param) if isinstance(constraint, gpytorch.constraints.Interval) else param)))
+					print(
+						"Parameter name: {}".format("".join(param_name.split("raw_"))),
+						utility.format_array("value", constraint.transform(param) if isinstance(constraint, gpytorch.constraints.Interval) else param)
+					)
 
 		def get_lr(optimizer: torch.optim.Optimizer) -> float:
 			"""
@@ -482,7 +440,7 @@ class GPRPredictors:
 		"""
 		for iElement in range(pes.NUM_ELM):
 			if check_predictor(self.predictors[iElement]):
-				print("Training " + get_RI_label(iElement))
+				print("Training " + utility.get_RI_label(iElement))
 				self.predictors[iElement].train()
 
 	def update_weights(self) -> None:

gpr/main.py
@@ -8,6 +8,7 @@ The main module.
 """
 
 import collections.abc
+import datetime
 import io
 import os
 import tarfile
@@ -30,6 +31,7 @@ import expectation
 import gp
 import pes
 import sample
+import utility
 
 FIGSIZE: tuple[float, float] = (6.4, 4.8)
 LIMIT: float = 1.0
@@ -38,7 +40,7 @@ LOCATOR: matplotlib.ticker.Locator = matplotlib.ticker.MaxNLocator(nbins=21)
 NORM: matplotlib.colors.Normalize = matplotlib.colors.CenteredNorm(0.0, LIMIT, True)
 NUM_PTS: typing.Literal[256] = 256
 NUM_XTR_RATIO: typing.Literal[50] = 50
-NUM_MC_PTS: typing.Literal[1_000_000] = 1_000_000
+NUM_MC_PTS: typing.Literal[10_000_000] = 10_000_000
 
 
 def read_input() -> tuple[npt.NDArray[np.double], npt.NDArray[np.double], npt.NDArray[np.double], float, float]:
@@ -158,57 +160,6 @@ def plot_region(arr: npt.NDArray[np.double]) -> npt.NDArray[np.double]:
 	return arr[::4, ::4]
 
 
-def format_array(arr_name: str, arr: npt.NDArray | float | int | complex) -> str:
-	"""
-	To print the flatten array as well as raw number
-
-	Parameters
-	----------
-	arr_name : str
-		The name of the array or variable
-	arr : npt.NDArray | float | int
-		The array or raw number
-
-	Returns
-	-------
-	str
-		_description_
-	"""
-	if isinstance(arr, np.ndarray):
-		return (arr_name + " =" + " {}" * arr.size).format(*arr.ravel())
-	elif isinstance(arr, complex):
-		return arr_name + " = {} + {}i".format(arr.real, arr.imag)
-	else:
-		return arr_name + " = " + str(arr)
-
-
-def dimension_name(DimIndex: int) -> str:
-	"""
-	Name corresponding to the dimension in phase space
-
-	Parameters
-	----------
-	DimIndex : int
-		Index of the dimension, range in [0, PHASEDIM)
-
-	Returns
-	-------
-	str
-		The name of the dimension
-	"""
-	assert 0 <= DimIndex < pes.PHASEDIM
-	if pes.DIM == 1:
-		if DimIndex == 0:
-			return "x"
-		else:
-			return "p"
-	else:
-		if DimIndex < pes.DIM:
-			return r"$x_{}$".format(DimIndex)
-		else:
-			return r"$p_{}$".format(DimIndex - pes.DIM)
-
-
 def main() -> None:
 	"""
 	The main routine
@@ -227,7 +178,14 @@ def main() -> None:
 	dt: float
 	mass, r0, sigma_r0, output_interval, dt = read_input()
 	output_steps: int = int(round(output_interval / dt))
-	print(format_array("Mass", mass), format_array("Initial center", r0), format_array("Initial deviation", sigma_r0), format_array("Time step", dt), format_array("Steps between output", output_steps), sep='\n')
+	print(
+		utility.format_array("Mass", mass),
+		utility.format_array("Initial center", r0),
+		utility.format_array("Initial deviation", sigma_r0),
+		utility.format_array("Time step", dt),
+		utility.format_array("Steps between output", output_steps),
+		sep='\n'
+	)
 	# read input
 	data: npt.NDArray[np.cdouble] = read_data()
 	total_ticks: int = data.shape[0]
@@ -277,6 +235,8 @@ def main() -> None:
 	PRM_FNAME: typing.Literal["parameters"] = "parameters"
 	LSS_FNAME: typing.Literal["loss"] = "loss"
 	AVE_FNAME: typing.Literal["ave"] = "ave"
+	DATA_EXTENSION: typing.Literal[".txt"] = ".txt"
+	FIGURE_EXTENSION: typing.Literal[".png"] = ".png"
 	pts_f: io.TextIOWrapper
 	den_f: io.TextIOWrapper
 	all_f: io.TextIOWrapper
@@ -284,15 +244,15 @@ def main() -> None:
 	scl_f: io.TextIOWrapper
 	prm_f: io.TextIOWrapper
 	lss_f: io.TextIOWrapper
-	with open("points.txt", 'w', encoding="UTF-8") as pts_f,\
-		open("belonging.txt", 'w', encoding="UTF-8") as bln_f,\
-		open("density.txt", 'w', encoding="UTF-8") as den_f,\
-		open("all_grids.txt", 'w', encoding="UTF-8") as all_f,\
-		open(ERR_FNAME + ".txt", 'w', encoding="UTF-8") as err_f,\
-		open(AVE_FNAME + ".txt", 'w', encoding="UTF-8") as ave_f,\
-		open(SCL_FNAME + ".txt", 'w', encoding="UTF-8") as scl_f,\
-		open(PRM_FNAME + ".txt", 'w', encoding="UTF-8") as prm_f,\
-		open(LSS_FNAME + ".txt", 'w', encoding="UTF-8") as lss_f:
+	with open("points" + DATA_EXTENSION, 'w', encoding="UTF-8") as pts_f,\
+		open("belonging" + DATA_EXTENSION, 'w', encoding="UTF-8") as bln_f,\
+		open("density" + DATA_EXTENSION, 'w', encoding="UTF-8") as den_f,\
+		open("all_grids" + DATA_EXTENSION, 'w', encoding="UTF-8") as all_f,\
+		open(ERR_FNAME + DATA_EXTENSION, 'w', encoding="UTF-8") as err_f,\
+		open(AVE_FNAME + DATA_EXTENSION, 'w', encoding="UTF-8") as ave_f,\
+		open(SCL_FNAME + DATA_EXTENSION, 'w', encoding="UTF-8") as scl_f,\
+		open(PRM_FNAME + DATA_EXTENSION, 'w', encoding="UTF-8") as prm_f,\
+		open(LSS_FNAME + DATA_EXTENSION, 'w', encoding="UTF-8") as lss_f:
 		def train_pred_draw(iTick: int) -> None:
 			"""
 			To train the parameter, doing prediction on all grids, and draw it
@@ -309,7 +269,7 @@ def main() -> None:
 			"""
 			# get scale
 			scale: npt.NDArray[np.double] = get_scale(gp_density)
-			print("Tick {}, scales = {}".format(iTick, scale))
+			print("Tick {}, {}, {}".format(iTick, utility.format_array("scales", scale), datetime.datetime.now()))
 			# interpolate the data
 			y_all: list[npt.NDArray[np.cdouble]] = [np.array([], dtype=np.cdouble) for _ in range(pes.NUM_TRIG)]
 			evolving_errors: npt.NDArray[np.double] = np.empty((pes.NUM_PES, pes.NUM_PES), np.double)
@@ -326,7 +286,6 @@ def main() -> None:
 						y_all[TrilIndex].imag = interpolator_im(gp_pts[TrilIndex])
 						evolving_errors[jPES, iPES] = np.sum((y_all[TrilIndex].real - gp_density[TrilIndex].real) ** 2) / num_points[TrilIndex]
 						evolving_errors[iPES, jPES] = np.sum((y_all[TrilIndex].imag - gp_density[TrilIndex].imag) ** 2) / num_points[TrilIndex]
-
 			evolving_errors = evolving_errors.reshape(-1)
 			# fit
 			predictors.update(gp_pts, gp_density, num_points, scale)
@@ -382,13 +341,13 @@ def main() -> None:
 							0.5,
 							"black"
 						)
-						ax.set_title("Rescaled {}\nRescale Factor = {:.6e}".format(title[0] + gp.get_RI_label(iElement), scale[iElement]))
+						ax.set_title("Rescaled {}\nRescale Factor = {:.6e}".format(title[0] + utility.get_RI_label(iElement), scale[iElement]))
 					elif iPlot == 1:
 						ax.set_title("Rescaled Error = {:.6e}\nRescaled Predict Error = {:.6e}".format(rescaled_errors[iElement], evolving_errors[iElement]))
 					elif iPlot == 3:
 						max_idx: np.intp = np.argmax(np.abs(diff[iElement]).reshape(-1))
 						ax.scatter(x_grids[max_idx // n_grids], p_grids[max_idx % n_grids], 15, "black")
-						ax.set_title("Rescaled {}\nRescaled Max Abs diff = {:.6e}".format(title[iPlot] + gp.get_RI_label(iElement), np.abs(diff[iElement])[max_idx // n_grids, max_idx % n_grids]))
+						ax.set_title("Rescaled {}\nRescaled Max Abs diff = {:.6e}".format(title[iPlot] + utility.get_RI_label(iElement), np.abs(diff[iElement])[max_idx // n_grids, max_idx % n_grids]))
 			fig.suptitle("Tick = {}\nTotal Rescaled Error = {}\nTotal Rescaled Predict Error = {}".format(iTick, np.sum(rescaled_errors), np.sum(evolving_errors)))
 			fig.savefig("Tick = {:03}.png".format(iTick))
 			# print to file
@@ -408,60 +367,40 @@ def main() -> None:
 				print(*aver.population(), *aver.coordinates(), *aver.covariance()[np.tril_indices(pes.PHASEDIM)], aver.potential(), aver.kinetic(mass), *aver.purity().reshape(-1), end=' ', file=ave_f)
 			print("", file=ave_f, flush=True)
 
-		def init() -> typing.Iterable[matplotlib.artist.Artist | typing.Iterable[matplotlib.artist.Artist]]:
+		def print_parameter_scale_loss(scale: npt.NDArray[np.double]) -> None:
 			"""
-			Initializer of the animation
+			To print parameters, rescale factor, and loss on sample points to file
 
-			Returns
-			-------
-			typing.Iterable[matplotlib.artist.Artist | typing.Iterable[matplotlib.artist.Artist]]
-				figure and axes
+			Parameters
+			----------
+			scale : npt.NDArray[np.double]
+				Rescale factor
 			"""
-			for iElement in range(pes.NUM_ELM):
-				for iPlot in range(NUM_PLOTS):
-					ax: matplotlib.axes.Axes = axs[iPlot, iElement]
-					ax.set_xlabel('x')
-					ax.set_ylabel('p')
-					ax.set_title("Rescaled " + title[iPlot] + gp.get_RI_label(iElement))
-			fig.colorbar(matplotlib.cm.ScalarMappable(cmap=CMAP, norm=NORM), ax=axs.ravel().tolist())
-			train_pred_draw(0)
 			predictors.print(prm_f)
 			print('\n', file=prm_f)
-			np.savetxt(scl_f, get_scale(gp_density))
+			np.savetxt(scl_f, scale)
 			print('\n', file=scl_f)
 			for i in range(pes.NUM_ELM):
 				print(predictors[i].error().item(), file=lss_f)
 			print('\n', file=lss_f)
-			return fig, axs
-
-		def draw(iTick: int) -> typing.Iterable[matplotlib.artist.Artist | typing.Iterable[matplotlib.artist.Artist]]:
-			"""
-			Implementation of each frame
-
-			Parameters
-			----------
-			iTick : int
-				Current time tick, to access grid data and for plotting
-
-			Returns
-			-------
-			typing.Iterable[matplotlib.artist.Artist | typing.Iterable[matplotlib.artist.Artist]]
-				figure and axes
-			"""
-			nonlocal gp_pts, gp_density, num_points
+			
+		for iElement in range(pes.NUM_ELM):
+			for iPlot in range(NUM_PLOTS):
+				ax: matplotlib.axes.Axes = axs[iPlot, iElement]
+				ax.set_xlabel('x')
+				ax.set_ylabel('p')
+				ax.set_title("Rescaled " + title[iPlot] + utility.get_RI_label(iElement))
+		fig.colorbar(matplotlib.cm.ScalarMappable(cmap=CMAP, norm=NORM), ax=axs.ravel().tolist())
+		train_pred_draw(0)
+		print_parameter_scale_loss(get_scale(gp_density))
+		for iTick in range(1, total_ticks):
 			# evolve
 			for _ in range(output_steps):
 				evolve.evolve(gp_pts, gp_density, mass, dt, predictors.predict)
 				evolve.sh_evolve(sh_pts, sh_density, sh_belonging_idx, mass, dt, predictors.predict)
 				scale: npt.NDArray[np.double] = get_scale(gp_density)
 				predictors.update(gp_pts, gp_density, num_points, scale)
-				predictors.print(prm_f)
-				print('\n', file=prm_f)
-				np.savetxt(scl_f, scale)
-				print('\n', file=scl_f)
-				for i in range(pes.NUM_ELM):
-					print(predictors[i].error().item(), file=lss_f)
-				print('\n', file=lss_f)
+				print_parameter_scale_loss(scale)
 			# update and predict
 			train_pred_draw(iTick)
 			# exchange with SH
@@ -474,42 +413,36 @@ def main() -> None:
 				for jPES in range(iPES + 1):
 					TrilIndex: int = pes.flatten_tril_index[iPES, jPES]
 					gp_density[TrilIndex][num_points[TrilIndex]:] = predictors.predict(gp_pts[TrilIndex][num_points[TrilIndex]:], iPES * pes.NUM_PES + jPES)
-			predictors.update(gp_pts, gp_density, num_points, get_scale(gp_density))
+			scale: npt.NDArray[np.double] = get_scale(gp_density)
+			predictors.update(gp_pts, gp_density, num_points, scale)
 			predictors.train()
-			predictors.print(prm_f)
-			print('\n', file=prm_f)
-			np.savetxt(scl_f, get_scale(gp_density))
-			print('\n', file=scl_f)
-			for i in range(pes.NUM_ELM):
-				print(predictors[i].error().item(), file=lss_f)
-			print('\n', file=lss_f)
-			return fig, axs
-
-		# draw animation
-		matplotlib.animation.FuncAnimation(fig, draw, range(1, total_ticks), init).save("gpr.gif", "imagemagick")
+			print_parameter_scale_loss(scale)
 
 	# then plots
 	plt.close(fig)
 	# error
 	error_average_ticks: npt.NDArray[np.double] = np.arange(total_ticks) * output_interval
 	fig, axs = plt.subplots(1, 3, figsize=(FIGSIZE[0] * 3, FIGSIZE[1]))
-	error: npt.NDArray[np.double] = np.loadtxt(ERR_FNAME + ".txt").reshape(-1, 3, pes.NUM_ELM)
+	error: npt.NDArray[np.double] = np.loadtxt(ERR_FNAME + DATA_EXTENSION).reshape(-1, 3, pes.NUM_ELM)
 	error_titles: list[str] = ["Original", "Rescaled", "Evolving"]
 	for iPlot in range(axs.size):
 		ax: matplotlib.axes.Axes = axs[iPlot]
 		for iElement in range(pes.NUM_ELM):
-			ax.semilogy(error_average_ticks, error[:, iPlot, iElement], label=gp.get_RI_label(iElement))
+			ax.semilogy(error_average_ticks, error[:, iPlot, iElement], label=utility.get_RI_label(iElement))
 		ax.set_xlabel("Time / a.u.")
 		ax.set_ylabel("Error")
 		ax.legend()
 		ax.set_title(error_titles[iPlot] + " Error")
-	fig.savefig(ERR_FNAME + ".png")
+	fig.savefig(ERR_FNAME + FIGURE_EXTENSION)
 	plt.close(fig)
 	# averages
 	ave_type_titles: list[str] = ["Monte Carlo", "Analytical"]
-	plot_titles = ["Population"] + [dimension_name(iDim) for iDim in range(pes.PHASEDIM)] + [("Cov(" + dimension_name(jDim) + ", " + dimension_name(iDim) + ")") if iDim != jDim else ("Var[" + dimension_name(iDim) + "]") for iDim in range(pes.PHASEDIM) for jDim in range(iDim + 1)] + ["Energy", "Purity"]
+	plot_titles: list[str] = ["Population"]\
+		+ [utility.dimension_name(iDim) for iDim in range(pes.PHASEDIM)]\
+		+ [("Cov(" + utility.dimension_name(jDim) + ", " + utility.dimension_name(iDim) + ")") if iDim != jDim else ("Var[" + utility.dimension_name(iDim) + "]") for iDim in range(pes.PHASEDIM) for jDim in range(iDim + 1)]\
+		+ ["Energy", "Purity"]
 	fig, axs = plt.subplots(len(ave_type_titles), len(plot_titles), figsize=(FIGSIZE[0] * len(plot_titles), FIGSIZE[1] * len(ave_type_titles))) # population, <x> and <p>, energy, purity
-	averages: npt.NDArray[np.double] = np.loadtxt(AVE_FNAME + ".txt")
+	averages: npt.NDArray[np.double] = np.loadtxt(AVE_FNAME + DATA_EXTENSION)
 	assert averages.shape[0] == total_ticks
 	averages = averages[:, 1:].reshape(total_ticks, 2, (averages.shape[1] - 1) // 2)
 	for iRow in range(axs.shape[0]):
@@ -535,44 +468,44 @@ def main() -> None:
 				y_label += " / a.u."
 			else: # purity
 				for iElement in range(pes.NUM_ELM):
-					ax.plot(error_average_ticks, averages[:, iRow, pes.NUM_PES + pes.PHASEDIM * (pes.PHASEDIM + 3) // 2 + 2 + iElement], label=gp.get_RI_label(iElement))
+					ax.plot(error_average_ticks, averages[:, iRow, pes.NUM_PES + pes.PHASEDIM * (pes.PHASEDIM + 3) // 2 + 2 + iElement], label=utility.get_RI_label(iElement))
 				ax.plot(error_average_ticks, np.sum(averages[:, iRow, pes.NUM_PES + pes.PHASEDIM * (pes.PHASEDIM + 3) // 2 + 2:], -1), label="Total")
 				ax.legend()
 			ax.set_xlabel("Time / a.u.")
 			ax.set_ylabel(y_label)
 			ax.set_title(ave_type_titles[iRow] + " Average of " + plot_titles[iCol])
-	fig.savefig(AVE_FNAME + ".png")
+	fig.savefig(AVE_FNAME + FIGURE_EXTENSION)
 	plt.close(fig)
 	# parameters
 	param_loss_scale_ticks: npt.NDArray[np.double] = np.concatenate([np.arange(i * output_steps, (i + 1) * output_steps + 1) for i in range(total_ticks - 1)]) * dt # This should be size of (total_ticks - 1) * (output_steps + 1)
 	fig, axs = plt.subplots(1, pes.PHASEDIM, figsize=(FIGSIZE[0] * pes.PHASEDIM, FIGSIZE[1]))
-	parameters: npt.NDArray[np.double] = np.loadtxt(PRM_FNAME + ".txt").reshape(-1, pes.NUM_ELM, pes.PHASEDIM)[:-1]
+	parameters: npt.NDArray[np.double] = np.loadtxt(PRM_FNAME + DATA_EXTENSION).reshape(-1, pes.NUM_ELM, pes.PHASEDIM)[:-1]
 	assert param_loss_scale_ticks.size == parameters.shape[0]
-	parameter_names: list[str] = [dimension_name(iDim) for iDim in range(pes.PHASEDIM)]
+	parameter_names: list[str] = [utility.dimension_name(iDim) for iDim in range(pes.PHASEDIM)]
 	for iDim in range(pes.PHASEDIM):
 		ax: matplotlib.axes.Axes = axs[iDim]
 		for iElement in range(pes.NUM_ELM):
-			ax.plot(param_loss_scale_ticks, np.sqrt(parameters[:, iElement, iDim]), label=gp.get_RI_label(iElement))
+			ax.plot(param_loss_scale_ticks, parameters[:, iElement, iDim], label=utility.get_RI_label(iElement))
 		ax.set_xlabel("Time / a.u.")
 		ax.set_ylabel("Characteristic Length / a.u.")
 		ax.set_title("Characteristic Length of " + parameter_names[iDim])
 		ax.legend()
-	fig.savefig(PRM_FNAME + ".png")
+	fig.savefig(PRM_FNAME + FIGURE_EXTENSION)
 	plt.close(fig)
 	# loss and rescale factor
-	loss_scale: npt.NDArray[np.double] = np.concatenate((np.loadtxt(LSS_FNAME + ".txt").reshape(-1, pes.NUM_ELM, 1), np.loadtxt(SCL_FNAME + ".txt").reshape(-1, pes.NUM_ELM, 1)), -1)[:-1]
+	loss_scale: npt.NDArray[np.double] = np.concatenate((np.loadtxt(LSS_FNAME + DATA_EXTENSION).reshape(-1, pes.NUM_ELM, 1), np.loadtxt(SCL_FNAME + DATA_EXTENSION).reshape(-1, pes.NUM_ELM, 1)), -1)[:-1]
 	assert param_loss_scale_ticks.size == loss_scale.shape[0]
 	loss_scale_titles: list[str] = ["Loss on Sample Points", "Rescale Factor"]
 	fig, axs = plt.subplots(1, 2, figsize=(FIGSIZE[0] * 2, FIGSIZE[1]))
 	for iPlot in range(axs.size):
 		ax: matplotlib.axes.Axes = axs[iPlot]
 		for iElement in range(pes.NUM_ELM):
-				ax.semilogy(param_loss_scale_ticks, loss_scale[:, iElement, iPlot], label=gp.get_RI_label(iElement))
+			ax.semilogy(param_loss_scale_ticks, loss_scale[:, iElement, iPlot], label=utility.get_RI_label(iElement))
 		ax.set_xlabel("Time / a.u.")
 		ax.set_ylabel("Characteristic Length / a.u." if iPlot < pes.PHASEDIM else "")
 		ax.set_title(loss_scale_titles[iPlot])
 		ax.legend()
-	fig.savefig(LSS_FNAME + "-" + SCL_FNAME + ".png")
+	fig.savefig(LSS_FNAME + "-" + SCL_FNAME + FIGURE_EXTENSION)
 	plt.close(fig)
 	# tar figures
 	with tarfile.open("ticks.tar.gz", "w:gz") as tf:

gpr/sample.py
@@ -11,6 +11,7 @@ import sklearn.cluster
 import torch
 
 import pes
+import utility
 
 VARIANCE_RATIO: float = 5.0
 np_rng: np.random.Generator = np.random.Generator(np.random.MT19937(0))
@@ -60,7 +61,15 @@ def sample_central_points(num_points: int | npt.NDArray[np.int_], all_points: li
 			TrilIndex: int = pes.flatten_tril_index[iPES, jPES]
 			kmeans: sklearn.cluster.KMeans = sklearn.cluster.KMeans(num_points[TrilIndex], init="k-means++", n_init="auto", random_state=np.random.RandomState(np_rng.bit_generator), algorithm="lloyd").fit(all_points[TrilIndex])
 			all_points[TrilIndex][:num_points[TrilIndex]] = kmeans.cluster_centers_
-			print("rho({}, {}), <r> = {}, stddev = {}".format(iPES, jPES, np.average(all_points[TrilIndex][:num_points[TrilIndex]], 0), np.std(all_points[TrilIndex][:num_points[TrilIndex]], 0)), flush=True)
+			print(
+				"rho({}, {}), {}, {}".format(
+					iPES,
+					jPES,
+					utility.format_array("<r>", np.average(all_points[TrilIndex][:num_points[TrilIndex]], 0)),
+					utility.format_array("stddev", np.std(all_points[TrilIndex][:num_points[TrilIndex]], 0))
+				),
+				flush=True
+			)
 
 
 def sample_extra_points(num_points: int | npt.NDArray[np.int_], all_points: list[npt.NDArray[np.double]]) -> None:
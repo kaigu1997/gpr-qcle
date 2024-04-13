Fix marginal calculation expectation.py: able to deal with 0 population element

gpr/evolve.py
@@ -1,7 +1,6 @@
 """
 evolve
 ======
-
 This module provides the adiabatic and non-adiabatic evolution scheme.
 """
 import enum
@@ -290,6 +289,7 @@ def evolve_density_non_adiabatically(
 				rho_part.imag[1] = rho_save[1].imag
 				rho_part.real[2] = (1.0 - cosphi) / 2.0 * rho_save[0].real + sinphi * rho_save[1].real + (1.0 + cosphi) / 2.0 * rho_save[2].real
 				rho_part.imag[2] = 0.0
+
 			# first step: (x0, p0) -> (x2, p1)
 			# second step, off-diagonal branching to p2, and broadcast to x3
 			# (backward) direction is included. So when evolve forward, the branch correspondence remains the same
@@ -396,7 +396,7 @@ def evolve_element_density(
 	non_adiabatic_indices: npt.NDArray[np.int_] = np.flatnonzero(IsCouplePerPoint) # non-adiabatic points
 	evolve_density_adiabatically(density.ravel()[adiabatic_indices], x0.reshape(-1, pes.DIM)[adiabatic_indices], x2.reshape(-1, pes.DIM)[adiabatic_indices], x4.reshape(-1, pes.DIM)[adiabatic_indices], evolve_element_density.drc, dt, RowIndex, ColIndex)
 	density[non_adiabatic_indices] = evolve_density_non_adiabatically(
-		density[non_adiabatic_indices],
+		density.ravel()[non_adiabatic_indices],
 		x4.reshape(-1, pes.DIM)[non_adiabatic_indices],
 		p2.reshape(-1, pes.DIM)[non_adiabatic_indices],
 		x2.reshape(-1, pes.DIM)[non_adiabatic_indices],

gpr/expectation.py
@@ -177,9 +177,9 @@ class MonteCarloAverage(Averager):
 		Parameters
 		----------
 		ref_pts : npt.NDArray[np.double]
-			_description_
+			Current points, used to estimate average and variance
 		predictor : typing.Callable[[npt.NDArray[np.double], int], npt.NDArray[np.cdouble]]
-			_description_
+			Used to predict the density of the points
 		"""
 		for iPES in range(pes.NUM_PES):
 			for jPES in range(iPES + 1):
@@ -252,7 +252,7 @@ class AnalyticalAverager(Averager):
 		for iPES in range(pes.NUM_PES):
 			ElementIndex: int = iPES * pes.NUM_PES + iPES
 			pred: gp.SinglePredictor = self._predictors[ElementIndex]
-			result[iPES] = pred.model.cov.lengthscale.prod().item() * pred.get_weights().sum().item() / self._predictors.scale[ElementIndex]
+			result[iPES] = pred.model.cov.lengthscale.prod().item() * pred.get_weights().sum().item()
 		return result * AnalyticalAverager.AVERAGE_CONSTANT
 
 	def coordinates(self) -> npt.NDArray[np.double]:
@@ -260,7 +260,7 @@ class AnalyticalAverager(Averager):
 		for iPES in range(pes.NUM_PES):
 			ElementIndex: int = iPES * pes.NUM_PES + iPES
 			pred: gp.SinglePredictor = self._predictors[ElementIndex]
-			result += pred.model.cov.lengthscale.prod().item() / self._predictors.scale[ElementIndex] * (pred.get_weights()[:, None] * pred.get_training_features()).sum(0).detach().numpy()
+			result += pred.model.cov.lengthscale.prod().item() * (pred.get_weights()[:, None] * pred.get_training_features()).sum(0).detach().numpy()
 		return result * AnalyticalAverager.AVERAGE_CONSTANT
 
 	def square_coordinates(self) -> npt.NDArray[np.double]:
@@ -268,7 +268,7 @@ class AnalyticalAverager(Averager):
 		for iPES in range(pes.NUM_PES):
 			ElementIndex: int = iPES * pes.NUM_PES + iPES
 			pred: gp.SinglePredictor = self._predictors[ElementIndex]
-			result += pred.model.cov.lengthscale.prod().item() / self._predictors.scale[ElementIndex] * (
+			result += pred.model.cov.lengthscale.prod().item() * (
 				(pred.get_weights()[:, None, None] * pred.get_training_features()[:, :, None] * pred.get_training_features()[:, None, :]).sum(0)
 				+ pred.get_weights().sum() * torch.diagflat(pred.model.cov.lengthscale ** 2)).detach().numpy()
 		return result * AnalyticalAverager.AVERAGE_CONSTANT
@@ -283,8 +283,9 @@ class AnalyticalAverager(Averager):
 		result: npt.NDArray[np.double] = np.empty((pes.NUM_PES, pes.NUM_PES), np.double)
 		for iPES in range(pes.NUM_PES):
 			for jPES in range(pes.NUM_PES):
-				pred: gp.SinglePredictor = self._predictors[iPES * pes.NUM_PES + jPES]
+				ElementIndex: int = iPES * pes.NUM_PES + jPES
+				pred: gp.SinglePredictor = self._predictors[ElementIndex]
 				model: gp.GP = copy.deepcopy(pred.model)
 				model.cov.lengthscale *= np.sqrt(2.0)
-				result[iPES, jPES] = (np.pi ** pes.DIM) * pred.model.cov.lengthscale.prod().item() * (pred.get_weights() @ model.cov(pred.get_training_features(), pred.get_training_features()) @ pred.get_weights()).item() / (self._predictors.scale[iPES * pes.NUM_PES + jPES] ** 2)
+				result[iPES, jPES] = (np.pi ** pes.DIM) * pred.model.cov.lengthscale.prod().item() * (pred.get_weights() @ model.cov(pred.get_training_features(), pred.get_training_features()) @ pred.get_weights()).item()
 		return PURITY_FACTOR * (result + result.T - np.diag(np.diag(result)))

gpr/gp.py
@@ -1,7 +1,6 @@
 """
 gp
 ==
-
 This module provides support for gaussian process (gp) regression.
 """
 import copy
@@ -17,7 +16,7 @@ import numpy as np
 import numpy.typing as npt
 import torch
 
-sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.dirname(__file__) + "/..")
 
 import pes
 import utility
@@ -100,7 +99,7 @@ class SinglePredictor:
 	get_weights()
 		To get the weights, :math:`K^{-1}y`
 	predict(x_test)
-		To predict the average
+		To predict the average estimation
 	error()
 		To get the error by comparing label with prediction
 	train()
@@ -116,12 +115,12 @@ class SinglePredictor:
 	NOISE: float = 1e-4
 
 	def __init__(self, kernel: gpytorch.kernels.Kernel):
-		self.DIM: int = kernel.lengthscale.numel()
 		self.kernel: gpytorch.kernels.Kernel = copy.deepcopy(kernel)
-		likelihood: gpytorch.likelihoods.FixedNoiseGaussianLikelihood = gpytorch.likelihoods.FixedNoiseGaussianLikelihood(torch.full((2,), SinglePredictor.NOISE))
+		likelihood: gpytorch.likelihoods.FixedNoiseGaussianLikelihood = gpytorch.likelihoods.FixedNoiseGaussianLikelihood(torch.full((2,), __class__.NOISE))
 		self.x_all: torch.Tensor = torch.Tensor()
 		self.y_all: torch.Tensor = torch.Tensor()
-		self.model: GP = GP(torch.zeros((2, self.DIM)), torch.zeros((2,)), likelihood, self.kernel)
+		self.scale: float = 1.0
+		self.model: GP = GP(torch.zeros((2, pes.PHASEDIM)), torch.zeros((2,)), likelihood, self.kernel)
 		self.model_param: dict[str, torch.Tensor] = copy.deepcopy(self.model.state_dict())
 		self.k_inv_y: torch.Tensor = torch.Tensor()
 		self.weights_updated: bool = False
@@ -160,7 +159,7 @@ class SinglePredictor:
 
 	def predict(self, x_test: torch.Tensor) -> torch.Tensor:
 		"""
-		Instance of prediction of subset of regressor (SR) / projected process (PP)
+		To predict the average estimation
 
 		Parameters
 		----------
@@ -171,6 +170,10 @@ class SinglePredictor:
 		-------
 		torch.Tensor, shape of (N,)
 			Corresponding validation/test targets based on noise-free SR/PP mean.
+
+		Notes
+		-----
+		Instance of prediction of subset of regressor (SR) / projected process (PP)
 		"""
 		if not self.weights_updated:
 			self.update_weights()
@@ -188,10 +191,10 @@ class SinglePredictor:
 			The sum of squared prediction error
 		"""
 		if use_weight:
-			return torch.sum((self.y_all - self.predict(self.x_all)) ** 2)
+			return torch.sum((self.y_all - self.predict(self.x_all)) ** 2) * (self.scale ** 2)
 		else:
 			kmn: torch.Tensor = self.model.cov(self.x_all, self.get_training_features()).to_dense()
-			return torch.sum((self.y_all - (kmn @ (linear_operator.utils.stable_pinverse(kmn) @ self.y_all)).to_dense()) ** 2)
+			return torch.sum((self.y_all - (kmn @ (linear_operator.utils.stable_pinverse(kmn) @ self.y_all)).to_dense()) ** 2) * (self.scale ** 2)
 
 	def train(self) -> None:
 		"""
@@ -260,7 +263,7 @@ class SinglePredictor:
 		self.weights_updated = False
 		assert isinstance(self.model.train_targets, torch.Tensor)
 		# train model
-		self.model.likelihood = gpytorch.likelihoods.FixedNoiseGaussianLikelihood(torch.full((self.get_training_features().shape[0],), SinglePredictor.NOISE))
+		self.model.likelihood = gpytorch.likelihoods.FixedNoiseGaussianLikelihood(torch.full((self.get_training_features().shape[0],), __class__.NOISE))
 		self.model.train()
 		self.model.likelihood.train()
 		self.model.load_state_dict(self.model_param)
@@ -272,7 +275,7 @@ class SinglePredictor:
 		loss.backward()
 		last_value: float = loss.item()
 		print_stuff(loss, optimizer, self.model, True, "Init")
-		for i in range(1, SinglePredictor.MAX_ITER + 1):
+		for i in range(1, __class__.MAX_ITER + 1):
 			# adjust lr
 			old_prm: dict[str, torch.Tensor] = copy.deepcopy(self.model.state_dict())
 			optimizer.step()
@@ -297,20 +300,24 @@ class SinglePredictor:
 						self.model.load_state_dict(old_prm)
 						loss = self.error(False)
 						break
-			if i % (SinglePredictor.MAX_ITER // 100) == 0:
+			if i % (__class__.MAX_ITER // 100) == 0:
 				print("Iter {} - Loss: {:.15e} - lr: {}".format(i, loss.item(), get_lr(optimizer)))
 				print_model(self.model, True)
 				print_model(self.model)
 			# stopping criteria
-			if (last_value - loss.item()) / max(abs(last_value), abs(loss.item()), 1.0) < SinglePredictor.FTOL:
+			if (last_value - loss.item()) / max(abs(last_value), abs(loss.item()), 1.0) < __class__.FTOL:
 				finish_early = True
 				print("Convergence: |f_i - f_{i+1}| <= FTOL")
 				print("Iter {} - Loss: {:.15e} - lr: {}".format(i, loss.item(), get_lr(optimizer)))
+				print_model(self.model, True)
+				print_model(self.model)
 				break
-			if np.sqrt(sum(torch.sum(param.grad ** 2).item() if param.grad is not None else 0.0 for param in self.model.parameters())) < SinglePredictor.GTOL:
+			if np.sqrt(sum(torch.sum(param.grad ** 2).item() if param.grad is not None else 0.0 for param in self.model.parameters())) < __class__.GTOL:
 				finish_early = True
 				print("Convergence: |Gradient| <= GTOL")
 				print("Iter {} - Loss: {:.15e} - lr: {}".format(i, loss.item(), get_lr(optimizer)))
+				print_model(self.model, True)
+				print_model(self.model)
 				break
 			optimizer = optimizer.__class__(self.model.parameters(), lr=get_lr(optimizer))
 			optimizer.zero_grad()
@@ -318,8 +325,10 @@ class SinglePredictor:
 			loss.backward()
 			# print_stuff(loss, optimizer, self.model, True, "\tlast = {}, ".format(last_value))
 		if not finish_early:
-			print("Iter {} - Loss: {:.15e} - lr: {}".format(SinglePredictor.MAX_ITER, last_value, [param["lr"] for param in optimizer.param_groups]))
 			print("Stop: Total No. iterations reached limit.")
+			print("Iter {} - Loss: {:.15e} - lr: {}".format(__class__.MAX_ITER, last_value, [param["lr"] for param in optimizer.param_groups]))
+			print_model(self.model, True)
+			print_model(self.model)
 		print_model(self.model)
 		print("", flush=True)
 		self.model_param = copy.deepcopy(self.model.state_dict())
@@ -328,6 +337,7 @@ class SinglePredictor:
 		self,
 		x_all: torch.Tensor,
 		y_all: torch.Tensor,
+		scale: float,
 		num_points: int
 	) -> None:
 		"""
@@ -336,59 +346,43 @@ class SinglePredictor:
 		Parameters
 		----------
 		x_all : torch.Tensor, of shape (num_points * (1 + NUM_XTR_RATIO), PHASEDIM)
-				All training inputs
+			All training inputs
 		y_all : torch.Tensor, of shape (num_points * (1 + NUM_XTR_RATIO))
-				All training targets
+			All training targets
+		scale : float
+			The scaling factor to increase
 		num_points : int
-				The number of points located at the front of all points that is used as the subset
+			The number of points located at the front of all points that is used as the subset
 		"""
-		assert x_all.shape[-1] == self.DIM
-		self.x_all = copy.deepcopy(x_all)
-		self.y_all = copy.deepcopy(y_all)
+		assert x_all.shape[-1] == pes.PHASEDIM and x_all.numel() == y_all.numel() * pes.PHASEDIM
+		self.x_all = copy.deepcopy(x_all.reshape(-1, pes.PHASEDIM))
+		self.y_all = copy.deepcopy(y_all.reshape(-1))
+		self.scale = scale
 		self.model.set_train_data(self.x_all[:num_points].detach(), self.y_all[:num_points].detach(), False)
 		self.weights_updated = False
 
-	def get_marginal(self, dimensions: list[int]) -> "SinglePredictor":
+	def get_marginal(self, dimensions: list[int], x_test: torch.Tensor) -> torch.Tensor:
 		"""
 		To get the marginal distribution of current gaussian process regression
 
 		Parameters
 		----------
 		dimensions : list[int]
-			The dimensions to be kept
+			The dimensions to be kept, must not have any repeat
+		x_test : torch.Tensor, shape of (N, len(dimensions))
+			Validation/Test inputs
 
 		Returns
 		-------
-		SinglePredictor
-			The GPR predictor of the reduced dimensions
+		torch.Tensor, shape of (N,)
+			Corresponding validation/test targets based on noise-free SR/PP mean.
 		"""
-		current_dim: int = self.get_training_features().shape[-1]
-		assert all(0 <= dim <= current_dim for dim in dimensions)
-		result: SinglePredictor = SinglePredictor(gpytorch.kernels.RBFKernel(len(dimensions)))
-		# set up its characteristic lengths, features, and reweighted labels
-		result.model.cov.lengthscale = self.model.cov.lengthscale.reshape(-1)[[i for i in range(current_dim) if i not in dimensions]]
-		rescale_factor: float = (2.0 * torch.pi) ** (len(dimensions) / 2.0) * self.model.cov.lengthscale.reshape(-1)[dimensions].prod().item()
-		result.update(self.x_all[..., dimensions], self.y_all * rescale_factor, self.get_training_features().shape[0])
-		return result
-
-
-def check_predictor(predictor: SinglePredictor) -> bool:
-	"""
-	To check if the predictor could be used for training / predicting
-
-	If no label is given, or all the labels are 0, training / predicting is not needed.
-
-	Parameters
-	----------
-	predictor : SinglePredictor
-		The predictor
-
-	Returns
-	-------
-	bool
-		Availability of training / predicting
-	"""
-	return isinstance(predictor.model.train_targets, torch.Tensor) and not torch.all(predictor.model.train_targets == 0)
+		if not self.weights_updated:
+			self.update_weights()
+		marginal_kernel: gpytorch.kernels.Kernel = gpytorch.kernels.RBFKernel(len(dimensions))
+		marginal_kernel.lengthscale = self.model.cov.lengthscale.reshape(1, pes.PHASEDIM)[:, dimensions]
+		prefactor: float = np.sqrt((2.0 * torch.pi) ** (pes.PHASEDIM - len(dimensions))) * self.model.cov.lengthscale[:, [i for i in range(pes.PHASEDIM) if i not in dimensions]].prod().item()
+		return prefactor * (marginal_kernel(x_test, self.get_training_features()[:, dimensions]) @ self.k_inv_y).to_dense()
 
 
 class GPRPredictors:
@@ -402,27 +396,46 @@ class GPRPredictors:
 
 	Methods
 	-------
+	check_predictor(predictor)
+		To check if the predictor could be used for training / predicting
 	update(x_all, y_all, num_pt, scale)
 		To update the training inputs and targets, as well as the rescale factor
 	train()
 		To train each predictor
 	predict(x_input, ElementIndex)
 		To predict test targets based on input and corresponding density matrix element
-	get_marginal(dimensions)
+	get_marginal(dimensions, x_input, ElementIndex)
 		To get the marginal distribution over given dimensions
 	print(f)
 		To print parameters to file
 	"""
+	@staticmethod
+	def check_predictor(predictor: SinglePredictor) -> bool:
+		"""
+		To check if the predictor could be used for training / predicting
+
+		If no label is given, or all the labels are 0, training / predicting is not needed.
+
+		Parameters
+		----------
+		predictor : SinglePredictor
+			The predictor
+
+		Returns
+		-------
+		bool
+			Availability of training / predicting
+		"""
+		return isinstance(predictor.model.train_targets, torch.Tensor) and not torch.all(predictor.model.train_targets == 0)
+
 	def __init__(self, parameter_initial_values: npt.NDArray[np.double] | None = None):
 		kernel: gpytorch.kernels.Kernel
-		self.DIM: int
 		if parameter_initial_values is not None:
-			self.DIM = parameter_initial_values.size
-			kernel = gpytorch.kernels.RBFKernel(self.DIM)
+			assert parameter_initial_values.size == pes.PHASEDIM
+			kernel = gpytorch.kernels.RBFKernel(pes.PHASEDIM)
 			kernel.lengthscale = torch.from_numpy(parameter_initial_values)
 		else:
-			self.DIM = pes.PHASEDIM # by default
-			kernel = gpytorch.kernels.RBFKernel(self.DIM)
+			kernel = gpytorch.kernels.RBFKernel(pes.PHASEDIM)
 		self.predictors: list[SinglePredictor] = [SinglePredictor(kernel) for _ in range(pes.NUM_ELM)]
 		self.scale: npt.NDArray[np.double] = np.ones(pes.NUM_ELM, np.double)
 
@@ -473,8 +486,9 @@ class GPRPredictors:
 			TrilIndex: int = pes.flatten_tril_index[RowIndex, ColIndex]
 			pred.update(
 				torch.from_numpy(x_all[TrilIndex]),
-				torch.from_numpy((y_all[TrilIndex].real if RowIndex <= ColIndex else y_all[TrilIndex].imag) * self.scale[iElement]),
-				num_points[TrilIndex],
+				torch.from_numpy(y_all[TrilIndex].real if RowIndex <= ColIndex else y_all[TrilIndex].imag),
+				self.scale[iElement],
+				num_points[TrilIndex]
 			)
 
 	def train(self) -> None:
@@ -482,7 +496,7 @@ class GPRPredictors:
 		To train each predictor
 		"""
 		for iElement in range(pes.NUM_ELM):
-			if check_predictor(self.predictors[iElement]):
+			if __class__.check_predictor(self.predictors[iElement]):
 				print("Training " + utility.get_RI_label(iElement))
 				self.predictors[iElement].train()
 
@@ -502,61 +516,49 @@ class GPRPredictors:
 		npt.NDArray[np.cdouble], shape of (...)
 			Density of the element of all test inputs
 		"""
-		x_test: torch.Tensor = torch.from_numpy(x_input.reshape(-1, self.DIM))
-
-		def call_single_predictor(pred: SinglePredictor) -> npt.NDArray[np.double]:
+		def call_single_predictor(pred: SinglePredictor, x_test: torch.Tensor) -> npt.NDArray[np.double]:
 			"""
 			To do prediction of a single predictor
 
 			Parameters
 			----------
-			pred : SinglePredictor, shape of (N, PHASEDIM)
+			pred : SinglePredictor
 				The predictor
+			x_test : torch.Tensor, shape of (N, PHASEDIM)
+				Test inputs
 
 			Returns
 			-------
 			npt.NDArray[np.double], shape of (N,)
 				Test targets by the predictor
 			"""
-			if check_predictor(pred):
+			if __class__.check_predictor(pred):
 				return pred.predict(x_test).detach().numpy()
 			else:
 				return np.zeros(x_test.shape[0], np.double)
 
-		def get_scale(scale: np.double) -> np.double:
-			"""
-			Return 1 if the scale is 0, else the value itself
-
-			Parameters
-			----------
-			scale : np.double
-				A non-negative value
-
-			Returns
-			-------
-			np.double
-				1 if the scale is 0, else the value itself
-			"""
-			if scale == 0.0:
-				return np.double(1.0)
-			else:
-				return scale
-
+		assert x_input.shape[-1] == pes.PHASEDIM and 0 <= ElementIndex < pes.NUM_ELM
+		x_test: torch.Tensor = torch.from_numpy(x_input.reshape(-1, pes.PHASEDIM))
 		RowIndex: int = ElementIndex // pes.NUM_PES
 		ColIndex: int = ElementIndex % pes.NUM_PES
 		result: npt.NDArray[np.cdouble] = np.empty(x_test.shape[0], np.cdouble)
 		if RowIndex == ColIndex:
-			result.real = call_single_predictor(self.predictors[ElementIndex]) / get_scale(self.scale[ElementIndex])
+			result.real = call_single_predictor(self.predictors[ElementIndex], x_test)
 			result.imag = 0
 		elif RowIndex > ColIndex:
-			result.real = call_single_predictor(self.predictors[ColIndex * pes.NUM_PES + RowIndex]) / get_scale(self.scale[ColIndex * pes.NUM_PES + RowIndex])
-			result.imag = call_single_predictor(self.predictors[ElementIndex]) / get_scale(self.scale[ElementIndex])
+			result.real = call_single_predictor(self.predictors[ColIndex * pes.NUM_PES + RowIndex], x_test)
+			result.imag = call_single_predictor(self.predictors[ElementIndex], x_test)
 		else: # RowIndex < ColIndex
-			result.real = call_single_predictor(self.predictors[ElementIndex]) / get_scale(self.scale[ElementIndex])
-			result.imag = -call_single_predictor(self.predictors[ColIndex * pes.NUM_PES + RowIndex]) / get_scale(self.scale[ColIndex * pes.NUM_PES + RowIndex])
+			result.real = call_single_predictor(self.predictors[ElementIndex], x_test)
+			result.imag = -call_single_predictor(self.predictors[ColIndex * pes.NUM_PES + RowIndex], x_test)
 		return result.reshape(x_input.shape[:-1])
 
-	def get_marginal(self, dimensions: int | typing.Iterable[int]) -> "GPRPredictors":
+	def get_marginal(
+		self,
+		dimensions: int | typing.Iterable[int],
+		x_input: npt.NDArray[np.double],
+		ElementIndex: int
+	) -> npt.NDArray[np.cdouble]:
 		"""
 		To get the marginal distribution of current gaussian process regressions
 
@@ -564,20 +566,59 @@ class GPRPredictors:
 		----------
 		dimensions : int | typing.Iterable[int]
 			The dimensions to be kept
+		x_input : npt.NDArray[np.double], shape of (..., len(dimensions))
+			Test inputs
+		ElementIndex : int
+			Index of the element
 
 		Returns
 		-------
-		GPRPredictors
-			The GPR predictor of the reduced dimensions
+		npt.NDArray[np.double], shape of (N,)
+			Marginal distribution on the inputs
 		"""
+		def call_single_predictor(pred: SinglePredictor, dims: list[int], x_test: torch.Tensor) -> npt.NDArray[np.double]:
+			"""
+			To do prediction of a single predictor
+
+			Parameters
+			----------
+			pred : SinglePredictor
+				The predictor
+			dims : list[int]
+				The dims to be kept
+			x_input : torch.Tensor, shape of (N, len(dimensions))
+				Test inputs
+
+			Returns
+			-------
+			npt.NDArray[np.double], shape of (N,)
+				Test targets by the predictor
+			"""
+			if __class__.check_predictor(pred):
+				return pred.get_marginal(dims, x_test).detach().numpy()
+			else:
+				return np.zeros(x_test.shape[0], np.double)
+
 		if isinstance(dimensions, int):
 			dimensions = [dimensions]
-		dimensions = list(set(dimensions)) # remove duplicate
-		result: GPRPredictors = GPRPredictors()
-		result.DIM = len(dimensions)
-		result.predictors = [pred.get_marginal(dimensions) for pred in self.predictors]
-		result.scale = self.scale.copy()
-		return result
+		else:
+			dimensions = list(set(dimensions)) # remove duplicate
+		assert all(0 <= dim <= pes.PHASEDIM for dim in dimensions)
+		assert x_input.shape[-1] == len(dimensions) and 0 <= ElementIndex < pes.NUM_ELM
+		x_test: torch.Tensor = torch.from_numpy(x_input.reshape(-1, len(dimensions)))
+		RowIndex: int = ElementIndex // pes.NUM_PES
+		ColIndex: int = ElementIndex % pes.NUM_PES
+		result: npt.NDArray[np.cdouble] = np.empty(x_test.shape[0], np.cdouble)
+		if RowIndex == ColIndex:
+			result.real = call_single_predictor(self.predictors[ElementIndex], dimensions, x_test)
+			result.imag = 0
+		elif RowIndex > ColIndex:
+			result.real = call_single_predictor(self.predictors[ColIndex * pes.NUM_PES + RowIndex], dimensions, x_test)
+			result.imag = call_single_predictor(self.predictors[ElementIndex], dimensions, x_test)
+		else: # RowIndex < ColIndex
+			result.real = call_single_predictor(self.predictors[ElementIndex], dimensions, x_test)
+			result.imag = -call_single_predictor(self.predictors[ColIndex * pes.NUM_PES + RowIndex], dimensions, x_test)
+		return result.reshape(x_input.shape[:-1])
 
 	def print(self, f: io.TextIOWrapper) -> None:
 		"""

gpr/main.py
@@ -262,7 +262,6 @@ def main(to_draw: bool, grid_solution_file: str) -> None:
 			if to_train:
 				predictors.train()
 			# predict and marginal distribution
-			marginal_dist: list[gp.GPRPredictors] = [predictors.get_marginal(iDim) for iDim in range(pes.PHASEDIM)]
 			marginal: npt.NDArray[np.double] = np.empty((pes.PHASEDIM, pes.NUM_PES, pes.NUM_PES, n_grids), np.double)
 			for iPES in range(pes.NUM_PES):
 				for jPES in range(iPES + 1):
@@ -273,7 +272,7 @@ def main(to_draw: bool, grid_solution_file: str) -> None:
 							pred_element = predictors.predict(grid_coord, ElementIndex).reshape((n_grids,) * pes.PHASEDIM)
 						finally:
 							pass
-					marginal_pred_element: npt.NDArray[np.cdouble] = np.array([marginal_dist[iDim].predict(grids_each_dim[iDim].reshape(-1, 1), ElementIndex) for iDim in range(pes.PHASEDIM)]) # shape of (PHASEDIM, N_GRIDS)
+					marginal_pred_element: npt.NDArray[np.cdouble] = np.array([predictors.get_marginal(iDim, grids_each_dim[iDim].reshape(-1, 1), ElementIndex) for iDim in range(pes.PHASEDIM)]) # shape of (PHASEDIM, N_GRIDS)
 					if iPES == jPES:
 						if pred is not None and pred_element is not None:
 							pred[iPES, jPES] = pred_element.real

gpr/plot.py
@@ -1,7 +1,7 @@
 """
 plot
 ====
-To plot all results
+This module plots all results
 """
 import argparse
 import collections.abc
@@ -22,7 +22,7 @@ import matplotlib.figure
 import matplotlib.pyplot as plt
 import matplotlib.ticker
 import matplotlib.transforms
-# import matplotlib.typing
+import matplotlib.typing
 import numpy as np
 import numpy.typing as npt
 import PIL.Image
@@ -166,17 +166,19 @@ def plot_average() -> None:
 	"""
 	To plot all averages available
 	"""
+	NUM_MOMENTS: typing.Literal[5] = pes.PHASEDIM * (pes.PHASEDIM + 3) // 2
 	ave_type_titles: list[str] = ["Monte Carlo", "Analytical"]
+	NUM_AVE_TYPES: int = len(ave_type_titles)
 	plot_titles: list[str] = ["Population"]\
 		+ [utility.dimension_name(iDim) for iDim in range(pes.PHASEDIM)]\
 		+ [("Cov(" + utility.dimension_name(jDim) + ", " + utility.dimension_name(iDim) + ")") if iDim != jDim else ("Var[" + utility.dimension_name(iDim) + "]") for iDim in range(pes.PHASEDIM) for jDim in range(iDim + 1)]\
 		+ ["Energy", "Purity"] # moments of 0th, 1st and 2nd order, and other (energy and purity)
 	fig: matplotlib.figure.Figure
 	axs: np.ndarray[collections.abc.Sequence[collections.abc.Sequence[matplotlib.axes.Axes]], np.dtype[np.object_]]
-	fig, axs = plt.subplots(len(ave_type_titles), len(plot_titles), figsize=(FIGSIZE[0] * len(plot_titles), FIGSIZE[1] * len(ave_type_titles))) # population, <x> and <p>, energy, purity
+	fig, axs = plt.subplots(NUM_AVE_TYPES, len(plot_titles), figsize=(FIGSIZE[0] * len(plot_titles), FIGSIZE[1] * NUM_AVE_TYPES)) # population, <x> and <p>, energy, purity
 	averages: npt.NDArray[np.double] = np.loadtxt(AVERAGE_FILENAME + DATA_EXTENSION)
 	ticks: npt.NDArray[np.double] = averages[:, 0]
-	averages = averages[:, 1:].reshape(-1, 2, (averages.shape[1] - 1) // 2)
+	averages = averages[:, 1:].reshape(ticks.size, NUM_AVE_TYPES, (averages.shape[1] - 1) // NUM_AVE_TYPES)
 	for iRow in range(axs.shape[0]):
 		for iCol in range(axs.shape[1]):
 			ax: matplotlib.axes.Axes = axs[iRow, iCol]
@@ -186,22 +188,22 @@ def plot_average() -> None:
 					ax.plot(ticks, averages[:, iRow, iPES], label="State " + str(iPES))
 				ax.plot(ticks, np.sum(averages[:, iRow, :pes.NUM_PES], -1), label="Total")
 				ax.legend()
-			elif iCol <= pes.PHASEDIM * (pes.PHASEDIM + 3) // 2: # <x>, <p>, and covariances
+			elif iCol <= NUM_MOMENTS: # <x>, <p>, and covariances
 				ax.plot(ticks, averages[:, iRow, iCol - 1 + pes.NUM_PES])
 				y_label += " / a.u."
 				if iCol > pes.PHASEDIM:
 					y_label += r"$^2$"
-			elif iCol == pes.PHASEDIM * (pes.PHASEDIM + 3) // 2 + 1: # energies
-				ax.plot(ticks, averages[:, iRow, pes.NUM_PES + pes.PHASEDIM * (pes.PHASEDIM + 3) // 2 + 1], label="Kinetic Energy")
+			elif iCol == NUM_MOMENTS + 1: # energies
+				ax.plot(ticks, averages[:, iRow, pes.NUM_PES + NUM_MOMENTS + 1], label="Kinetic Energy")
 				if iRow == 0: # only mc has potential and thus total energy
-					ax.plot(ticks, averages[:, iRow, pes.NUM_PES + pes.PHASEDIM * (pes.PHASEDIM + 3) // 2], label="Potential Energy")
-					ax.plot(ticks, np.sum(averages[:, iRow, pes.NUM_PES + pes.PHASEDIM * (pes.PHASEDIM + 3) // 2:pes.NUM_PES + pes.PHASEDIM * (pes.PHASEDIM + 3) // 2 + 2], -1), label="Total Energy")
+					ax.plot(ticks, averages[:, iRow, pes.NUM_PES + NUM_MOMENTS], label="Potential Energy")
+					ax.plot(ticks, np.sum(averages[:, iRow, pes.NUM_PES + NUM_MOMENTS:pes.NUM_PES + NUM_MOMENTS + 2], -1), label="Total Energy")
 				ax.legend()
 				y_label += " / a.u."
 			else: # purity
 				for iElement in range(pes.NUM_ELM):
-					ax.plot(ticks, averages[:, iRow, pes.NUM_PES + pes.PHASEDIM * (pes.PHASEDIM + 3) // 2 + 2 + iElement], label=utility.get_RI_label(iElement))
-				ax.plot(ticks, np.sum(averages[:, iRow, pes.NUM_PES + pes.PHASEDIM * (pes.PHASEDIM + 3) // 2 + 2:], -1), label="Total")
+					ax.plot(ticks, averages[:, iRow, pes.NUM_PES + NUM_MOMENTS + 2 + iElement], label=utility.get_RI_label(iElement))
+				ax.plot(ticks, np.sum(averages[:, iRow, pes.NUM_PES + NUM_MOMENTS + 2:], -1), label="Total")
 				ax.legend()
 			ax.set_xlabel("Time / a.u.")
 			ax.set_ylabel(y_label)
@@ -565,19 +567,20 @@ class DensityMatrixDrawer:
 			ax.contourf(self.xv, self.pv, data[::self.col_divisor, ::self.row_divisor].T, levels=self.LEVEL, cmap=__class__.CMAP, norm=self.NORM)
 			if title is not None:
 				ax.set_title(title)
-			if central_points is not None:
-				ax.scatter(
-					np.clip(central_points[0], self.x_grids[0], self.x_grids[-1]),
-					np.clip(central_points[1], self.p_grids[0], self.p_grids[-1]),
-					0.5,
-					"black"
-				)
+			# scatter the central points on top
 			if extra_points is not None:
 				ax.scatter(
 					np.clip(extra_points[0], self.x_grids[0], self.x_grids[-1]),
 					np.clip(extra_points[1], self.p_grids[0], self.p_grids[-1]),
 					0.1,
 					"green"
+				)
+			if central_points is not None:
+				ax.scatter(
+					np.clip(central_points[0], self.x_grids[0], self.x_grids[-1]),
+					np.clip(central_points[1], self.p_grids[0], self.p_grids[-1]),
+					0.5,
+					"black"
 				)
 
 		pred_data: npt.NDArray[np.double]
@@ -691,7 +694,7 @@ class WavefunctionPlotter:
 	PICNAME : typing.Literal["wfn_{{:0{}}}.png"]
 		The template for pictures
 	"""
-	# WFN_COLORS: list[matplotlib.typing.ColorType]
+	WFN_COLORS: list[matplotlib.typing.ColorType]
 	if pes.NUM_PES < 10:
 		WFN_COLORS = matplotlib.color_sequences["Set1"][:pes.NUM_PES]
 	else:
@@ -733,7 +736,7 @@ class WavefunctionPlotter:
 		self.fig: matplotlib.figure.Figure
 		self.axs: np.ndarray[collections.abc.Sequence[matplotlib.axes.Axes], np.dtype[np.object_]]
 		self.fig, self.axs = plt.subplots(pes.DIM, 2, figsize=(FIGSIZE[0] * 2, FIGSIZE[1] * pes.DIM))
-		self.axs = self.axs.reshape(pes.DIM, 2)
+		self.axs = self.axs.reshape(pes.DIM, 2) # guaranteen it is matrix in case pes.DIM == 1
 		self.fig.suptitle(self.title)
 		labels: list[str] = [utility.dimension_name(iDim) for iDim in range(pes.PHASEDIM)]
 		titles: list[str] = ["Position", "Momentum"]
@@ -817,6 +820,7 @@ def main() -> None:
 	parser.add_argument("--animation", "--ani", "-a", default=0, type=int, help="Draw animation; if so, provide the interval between frames in ms. Draw frame by frame and compress into tarfile will always be done.")
 	parser.add_argument("--scattered", "-s", action="store_true", help="Scatter points on the distribution")
 	parser.add_argument("--rescaled", "-r", action="store_true", help="Rescale all elements to same (maximum = 1.0)")
+	parser.add_argument("--wavefunction-rescaled", "--wr", action="store_true", help="Rescale wavefunction as well (default to be the same as --rescaled)")
 	parser.add_argument("--density-matrix", "--dm", "-d", default=ALL_GRIDS_FILENAME + DATA_EXTENSION, type=str, help="File containing partial Wigner-transformed density matrix from Gaussian process regression")
 	parser.add_argument("--marginal", "-m", default=MARGINAL_FILENAME + DATA_EXTENSION, type=str, help="File containing marginal distribution from Gaussian process regression")
 	parser.add_argument("--grid-solution", "--grid", "-g", default="", type=str, help="File containing partial Wigner-transformed density matrix from grid solution")
@@ -866,7 +870,7 @@ def main() -> None:
 			output_interval,
 			r0=r0,
 			marginal_data=marginal_data.diagonal(axis1=2, axis2=3).swapaxes(-1, -2),
-			draw_rescaled=result["rescaled"]
+			draw_rescaled=result["wavefunction_rescaled"]
 		)
 		assert wfn_plotter.wfn_sqnm is not None
 		for iframe in range(wfn_plotter.wfn_sqnm.shape[0]):